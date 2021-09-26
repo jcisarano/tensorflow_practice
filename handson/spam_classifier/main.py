82c4da9bd55d866af3836e7cb6e527d70488a333
jcisarano <cisarano@gmail.com>
2021-09-26 12:00:01 -0400

    added functions to classify and count email structures

--- handson/spam_classifier/main.py
+++ handson/spam_classifier/main.py
@@ -1,6 +1,7 @@
 import os
 import email
 import email.policy
+from collections import Counter
 
 import numpy as np
 import pandas as pd
@@ -21,6 +22,26 @@ def load_emails(filename, path):
         return email.parser.BytesParser(policy=email.policy.default).parse(f)
 
 
+def get_email_structure(email):
+    if isinstance(email, str):
+        return email
+    payload = email.get_payload()
+    if isinstance(payload, list):
+        return "multiplart({})".format(", ".join([
+            get_email_structure(sub_email)
+            for sub_email in payload
+        ]))
+    else:
+        return email.get_content_type()
+
+def structures_counter(emails):
+    structures = Counter()
+    for email in emails:
+        structure = get_email_structure(email)
+        structures[structure] += 1
+    return structures
+
+
 DATASET_PATH: str = os.path.join("datasets")
 SPAM_PATH: str = os.path.join(DATASET_PATH, "spam")
 HAM_PATH: str = os.path.join(DATASET_PATH, "ham")
@@ -29,8 +50,6 @@ HAM_PATH: str = os.path.join(DATASET_PATH, "ham")
 if __name__ == '__main__':
     spam_filenames = load_filenames_in_directory(SPAM_PATH)
     ham_filenames = load_filenames_in_directory(HAM_PATH)
-
-    print(len(ham_filenames))
 
     ham_emails = [load_emails(name, HAM_PATH) for name in ham_filenames]
     spam_emails = [load_emails(name, SPAM_PATH) for name in spam_filenames]
