d96d0b869b4cd5ab8bb405d2e694589cdd3660ab
jcisarano <cisarano@gmail.com>
2021-09-26 11:47:58 -0400

    now load emails

--- handson/spam_classifier/main.py
+++ handson/spam_classifier/main.py
@@ -1,4 +1,6 @@
 import os
+import email
+import email.policy
 
 import numpy as np
 import pandas as pd
@@ -14,6 +16,11 @@ def load_filenames_in_directory(path):
     return f
 
 
+def load_emails(filename, path):
+    with open(os.path.join(path, filename), "rb") as f:
+        return email.parser.BytesParser(policy=email.policy.default).parse(f)
+
+
 DATASET_PATH: str = os.path.join("datasets")
 SPAM_PATH: str = os.path.join(DATASET_PATH, "spam")
 HAM_PATH: str = os.path.join(DATASET_PATH, "ham")
@@ -23,6 +30,10 @@ if __name__ == '__main__':
     spam_filenames = load_filenames_in_directory(SPAM_PATH)
     ham_filenames = load_filenames_in_directory(HAM_PATH)
 
-print(len(spam_filenames))
-print(len(ham_filenames))
+    print(len(ham_filenames))
+
+    ham_emails = [load_emails(name, HAM_PATH) for name in ham_filenames]
+    spam_emails = [load_emails(name, SPAM_PATH) for name in spam_filenames]
+
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
