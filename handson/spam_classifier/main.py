871badc6c8419e309522f148fb9082c412930ca9
jcisarano <cisarano@gmail.com>
2021-09-28 08:44:21 -0400

    run against test data

--- handson/spam_classifier/main.py
+++ handson/spam_classifier/main.py
@@ -12,6 +12,7 @@ import nltk
 from scipy.sparse import csr_matrix
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import precision_score, recall_score
 
 import re
 from html import unescape
@@ -206,4 +207,11 @@ if __name__ == '__main__':
     score = cross_val_score(log_clf, X_train_transformed, y_train, cv=3, verbose=3)
     print(score.mean())
 
+    X_test_transformed = preprocess_pipeline.transform(X_test)
+    log_clf = LogisticRegression(solver="liblinear", random_state=42)
+    log_clf.fit(X_train_transformed, y_train)
+
+    y_pred = log_clf.predict(X_test_transformed)
+    print("Precision: {:.2f}%".format(100 * precision_score(y_test, y_pred)))
+    print("Recall: {:.2f}%".format(100 * recall_score(y_test, y_pred)))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
