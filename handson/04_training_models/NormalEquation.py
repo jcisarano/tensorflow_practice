70b3ce20fbceb349b2a8f0de43a6f283bf03d29d
jcisarano <cisarano@gmail.com>
2021-10-04 17:43:23 -0400

    stochastic gradient descent

--- handson/04_training_models/NormalEquation.py
+++ handson/04_training_models/NormalEquation.py
@@ -35,8 +35,9 @@ def do_linear_regression(X_train, y_train, X_test):
 
 def do_linear_regression_with_batch_gd(X, y, eta=0.1, n_iterations=1000, m=100):
     theta = np.random.randn(2, 1)
+    X_b = np.c_[np.ones(X.shape), X]
     for iteration in range(n_iterations):
-        gradients = calc_theta_best(X, y)
+        gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
         theta = theta - eta * gradients
     return theta
 
@@ -57,6 +58,41 @@ def plot_gradient_descent(X_train, y_train, X_train_b, X_test, X_test_b, theta, 
     plt.xlabel("$x_1$", fontsize=18)
     plt.axis([0, 2, 0, 15])
     plt.title(r"$\eta = {}$".format(eta), fontsize=16)
+
+
+def learning_schedule(t, t0, t1):
+    return t0 / (t + t1)
+
+
+def stochastic_gradient_descent(X, y, X_test, theta, m, theta_path_sgd, n_epochs=50, t0=5, t1=50):
+    X_b = np.c_[np.ones((X.shape)), X]
+    X_test_b = np.c_[np.ones(X_test.shape), X_test]
+    for epoch in range(n_epochs):
+        for i in range(m):
+            if epoch == 0 and i < 20:
+                y_predict = X_test_b.dot(theta)
+                style = "b-" if i > 0 else "r--"
+                plt.plot(X_test, y_predict, style)
+            random_index = np.random.randint(m)
+            xi = X_b[random_index:random_index+1]
+            yi = y[random_index:random_index+1]
+            gradients = 2*xi.T.dot(xi.dot(theta) - yi)
+            eta = learning_schedule(epoch*m+i, t0, t1)
+            theta = theta - eta*gradients
+            theta_path_sgd.append(theta)
+    plt.plot(X, y, "b.")
+    plt.xlabel("$X_1$", fontsize=18)
+    plt.ylabel("$y$", rotation=0, fontsize=18)
+    plt.axis([0, 2, 0, 15])
+    plt.show()
+
+
+def do_sgd(X, y):
+    from sklearn.linear_model import SGDRegressor
+    sgd_reg = SGDRegressor(max_iter=50, tol=-np.infty, penalty=None, eta0=0.1, random_state=42)
+    sgd_reg.fit(X, y.ravel())
+    print(sgd_reg.intercept_, sgd_reg.coef_)
+
 
 def run():
     X, y = generate_data()
@@ -98,3 +134,12 @@ def run():
     plt.subplot(133)
     plot_gradient_descent(X, y, X_with_bias_column, X_new, X_new_b, theta, eta=0.5)
     plt.show()
+
+    # stochastic gradient descent
+    theta_path_sgd = []
+    m = len(X_with_bias_column)
+    np.random.seed(42)
+    theta = np.random.randn(2, 1)
+
+    stochastic_gradient_descent(X, y, X_new, theta, m, theta_path_sgd, n_epochs=50, t0=5, t1=50)
+    do_sgd(X ,y)
