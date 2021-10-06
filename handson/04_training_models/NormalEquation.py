462ae734c37c40d6e6e4f19103c796f6d4a7fb68
jcisarano <cisarano@gmail.com>
2021-10-06 18:12:46 -0400

    moved i-v plot to function

--- handson/04_training_models/NormalEquation.py
+++ handson/04_training_models/NormalEquation.py
@@ -366,6 +366,27 @@ def plot_log_regression():
     plt.show()
 
 
+def iris_virginica_fancy_plot(model, X, y):
+    X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
+    y_prob = model.predict_proba(X_new)  # predict_proba() gives the probability of each output, 0 and 1, so (p0, p1)
+    decision_boundary = X_new[y_prob[:, 1] >= 0.5][0]
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(X[y == 0], y[y == 0], "bs")
+    plt.plot([decision_boundary, decision_boundary], [-1, 2], "k:", linewidth=2)
+    plt.plot(X_new, y_prob[:, 1], "g-", linewidth=2, label="Iris-Virginica")
+    plt.plot(X_new, y_prob[:, 0], "b--", linewidth=2, label="Not Iris-Virginica")
+    plt.text(decision_boundary + 0.02, 0.15, "Decision boundary", fontsize=14, color="k", ha="center")
+    plt.arrow(decision_boundary, 0.08, -0.3, 0, head_width=0.05, head_length=0.1, fc="b", ec="b")
+    plt.arrow(decision_boundary, 0.92, 0.3, 0, head_width=0.05, head_length=0.1, fc="g", ec="g")
+    plt.xlabel("Petal width (cm)", fontsize=14)
+    plt.ylabel("Probability", fontsize=14)
+    plt.legend(loc="center left", fontsize=14)
+    plt.axis([0, 3, -0.02, 1.02])
+
+    plt.show()
+
+
 def run():
     X, y = generate_data()
     # plot_data(X, y)
@@ -546,21 +567,4 @@ def run():
     log_reg = LogisticRegression(solver="liblinear", random_state=42)
     log_reg.fit(X, y)
 
-    X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
-    y_prob = log_reg.predict_proba(X_new)  # predict_proba() gives the probability of each output, 0 and 1, so (p0, p1)
-    decision_boundary = X_new[y_prob[:, 1] >= 0.5][0]
-
-    plt.figure(figsize=(8, 5))
-    plt.plot(X[y == 0], y[y == 0], "bs")
-    plt.plot([decision_boundary, decision_boundary], [-1, 2], "k:", linewidth=2)
-    plt.plot(X_new, y_prob[:, 1], "g-", linewidth=2, label="Iris-Virginica")
-    plt.plot(X_new, y_prob[:, 0], "b--", linewidth=2, label="Not Iris-Virginica")
-    plt.text(decision_boundary + 0.02, 0.15, "Decision boundary", fontsize=14, color="k", ha="center")
-    plt.arrow(decision_boundary, 0.08, -0.3, 0, head_width=0.05, head_length=0.1, fc="b", ec="b")
-    plt.arrow(decision_boundary, 0.92, 0.3, 0, head_width=0.05, head_length=0.1, fc="g", ec="g")
-    plt.xlabel("Petal width (cm)", fontsize=14)
-    plt.ylabel("Probability", fontsize=14)
-    plt.legend(loc="center left", fontsize=14)
-    plt.axis([0, 3, -0.02, 1.02])
-
-    plt.show()
+    iris_virginica_fancy_plot(log_reg, X, y)
