89c4d1c049715eaf74c4dde6dd58aa00454ef8d5
jcisarano <cisarano@gmail.com>
2021-10-06 18:55:42 -0400

    plot i-v probs based on l-w

--- handson/04_training_models/NormalEquation.py
+++ handson/04_training_models/NormalEquation.py
@@ -387,6 +387,41 @@ def iris_virginica_fancy_plot(model, X, y):
     plt.show()
 
 
+def logistic_regression_plot(iris):
+    from sklearn.linear_model import LogisticRegression
+    X = iris["data"][:, (2, 3)]  # now use petal length and width
+    y = (iris["target"] == 2).astype(np.int)
+    log_reg = LogisticRegression(solver="liblinear", C=10 ** 10, random_state=42)
+    log_reg.fit(X, y)
+
+    x0, x1 = np.meshgrid(
+        np.linspace(2.9, 7, 500).reshape(-1, 1),
+        np.linspace(0.8, 7, 200).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]  # c_() translates slice objects to concatenation along the second axis.
+    y_proba = log_reg.predict_proba(X_new)
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(X[y == 0, 0], X[y == 0, 1], "bs")
+    plt.plot(X[y == 1, 0], X[y == 1, 1], "g^")
+
+    zz = y_proba[:, 1].reshape(x0.shape)
+    contour = plt.contour(x0, x1, zz, cmap=plt.cm.brg)
+
+    left_right = np.array([2.9, 7])
+    boundary = -(log_reg.coef_[0][0] * left_right + log_reg.intercept_[0]) / log_reg.coef_[0][1]
+
+    plt.clabel(contour, inline=1, fontsize=12)
+    plt.plot(left_right, boundary, "k--", linewidth=3)
+    plt.text(3.5, 1.5, "Not Iris-Virginica", fontsize=14, color="b", ha="center")
+    plt.text(6.5, 2.3, "Iris-Virginica", fontsize=14, color="g", ha="center")
+    plt.xlabel("Petal length", fontsize=14)
+    plt.ylabel("Petal width", fontsize=14)
+    plt.axis([2.9, 7, 0.8, 2.7])
+    plt.show()
+    plt.show()
+
+
 def run():
     X, y = generate_data()
     # plot_data(X, y)
@@ -567,4 +602,7 @@ def run():
     log_reg = LogisticRegression(solver="liblinear", random_state=42)
     log_reg.fit(X, y)
 
-    iris_virginica_fancy_plot(log_reg, X, y)
+    # iris_virginica_fancy_plot(log_reg, X, y)
+    print(log_reg.predict([[1.7], [1.5]]))
+
+    logistic_regression_plot(iris)
