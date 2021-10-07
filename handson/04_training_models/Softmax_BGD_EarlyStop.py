a64fc24d5a0d4774b2fcc2fe4b4c82ea710ebff9
jcisarano <cisarano@gmail.com>
2021-10-07 18:16:39 -0400

    added early stopping

--- handson/04_training_models/Softmax_BGD_EarlyStop.py
+++ handson/04_training_models/Softmax_BGD_EarlyStop.py
@@ -104,3 +104,38 @@ def run():
     accuracy_score = np.mean(y_predict == y_validation)
     print(accuracy_score)
 
+    # train again, but add early stop:
+    eta = 0.1
+    n_iterations = 5001
+    m = len(X_train)
+    epsilon = 1e-7
+    alpha = 0.1
+    best_loss = np.infty
+
+    Theta = np.random.randn(n_inputs, n_outputs)
+
+    for iteration in range(n_iterations):
+        logits = X_train.dot(Theta)
+        y_proba = softmax(logits)
+        xentropy_loss = -np.mean(np.sum(y_train_one_hot * np.log(y_proba + epsilon), axis=1))
+        l2_loss = 1/2 * np.sum(np.square(Theta[1:]))
+        loss = xentropy_loss + alpha * l2_loss
+        error = y_proba - y_train_one_hot
+        gradients = 1/m * X_train.T.dot(error) + np.r_[np.zeros([1, n_outputs]), alpha * Theta[1:]]
+        Theta = Theta - eta * gradients
+
+        logits = X_validation.dot(Theta)
+        y_proba = softmax(logits)
+        xentropy_loss = -np.mean(np.sum(y_validation_one_hot * np.log(y_proba + epsilon), axis=1))
+        l2_loss = 1/2 * np.sum(np.square(Theta[1:]))
+        loss = xentropy_loss + alpha * l2_loss
+        if iteration % 500 == 0:
+            print(iteration, loss)
+        if loss < best_loss:
+            best_loss = loss
+        else:
+            print(iteration - 1, best_loss)
+            print(iteration, loss, "early stopping")
+            break
+
+
