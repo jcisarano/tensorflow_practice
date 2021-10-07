244084dbc662cc736bedcbbca734828dac8e60b0
jcisarano <cisarano@gmail.com>
2021-10-06 22:31:57 -0400

    softmax example plot

--- handson/04_training_models/NormalEquation.py
+++ handson/04_training_models/NormalEquation.py
@@ -421,6 +421,46 @@ def logistic_regression_plot(iris):
     plt.show()
 
 
+def plot_softmax(iris):
+    from sklearn.linear_model import LogisticRegression
+    X = iris["data"][:, (2, 3)]  # petal length & width
+    y = iris["target"]  # unchanged targets
+    softmax_reg = LogisticRegression(multi_class="multinomial", solver="lbfgs", C=10, random_state=42)
+    softmax_reg.fit(X, y)
+    print(softmax_reg.predict([[5, 2]]))
+    print(softmax_reg.predict_proba([[5, 2]]))
+
+    x0, x1 = np.meshgrid(
+        np.linspace(0, 8, 500).reshape(-1, 1),
+        np.linspace(0, 3.5, 200).reshape((-1, 1)),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+
+    y_proba = softmax_reg.predict_proba(X_new)
+    y_predict = softmax_reg.predict(X_new)
+
+    zz1 = y_proba[:, 1].reshape(x0.shape)
+    zz = y_predict.reshape(x0.shape)
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(X[y == 2, 0], X[y == 2, 1], "g^", label="Iris-Virginica")
+    plt.plot(X[y == 1, 0], X[y == 1, 1], "bs", label="Iris-Versicolor")
+    plt.plot(X[y == 0, 0], X[y == 0, 1], "yo", label="Iris-Setosa")
+
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#fafab0', '#9898ff', '#a0faa0'])
+
+    plt.contourf(x0, x1, zz, cmap=custom_cmap)  # displays bg colors
+    contour = plt.contour(x0, x1, zz1, cmap=plt.cm.brg)  # displays curved lines
+    plt.clabel(contour, inline=1, fontsize=12)  # adds labels to lines
+    plt.xlabel("Petal length", fontsize=14)
+    plt.ylabel("Petal width", fontsize=14)
+    plt.legend(loc="center left", fontsize=14)
+    plt.axis([0, 7, 0, 3.5])
+    plt.show()
+
+
+
 def run():
     X, y = generate_data()
     # plot_data(X, y)
@@ -604,4 +644,5 @@ def run():
     # iris_virginica_fancy_plot(log_reg, X, y)
     print(log_reg.predict([[1.7], [1.5]]))
 
-    logistic_regression_plot(iris)
+    #logistic_regression_plot(iris)
+    plot_softmax(iris)
