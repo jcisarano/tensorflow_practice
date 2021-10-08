7c9b1d917ab7ed9971b2d51b84e73a0e09329c08
jcisarano <cisarano@gmail.com>
2021-10-08 18:25:18 -0400

    started classification training

---
+++ ztm/02_nn_classification_w_tf/main.py
@@ -0,0 +1,25 @@
+# Intro to NN classification with TensorFlow
+# How to write NN for classification problems, where you need to classify something as one thing or another
+# Types of classification:
+# Binary classification
+# Multiclass classification
+# Multilabel classification
+
+from sklearn.datasets import make_circles
+import pandas as pd
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # make 1000 examples
+    n_samples = 1000
+    X, y = make_circles(n_samples,
+                        noise=0.03,
+                        random_state=42)
+    # Check out features and labels:
+    print(X[:10])
+    print(y[:10])
+
+    circles = pd.DataFrame({"X0:": X[:, 0], "X1": X[:, 1], "label": y})
+    print(circles)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
