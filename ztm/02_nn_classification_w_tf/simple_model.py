a347a3d514084151152a1b08f18617798f9bc0d2
jcisarano <cisarano@gmail.com>
2021-10-10 16:08:38 -0400

    add function to plot decision boundaries

--- ztm/02_nn_classification_w_tf/simple_model.py
+++ ztm/02_nn_classification_w_tf/simple_model.py
@@ -1,5 +1,5 @@
 # simple starting model
-
+import numpy as np
 from sklearn.datasets import make_circles
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,6 +11,54 @@ import tensorflow as tf
 # 3. Compile the model: loss function, optimizer, metrics
 # 4. Fit the model to the training data
 # 5. Evaluate and improve through experimentation
+
+
+"""
+Function to help visualize predictions;
+params: trained model and X,y predictions
+create meshgrid of X values
+make predictions across meshgrid
+plot predictions along with lines along decision zone boundaries
+"""
+def plot_decision_boundary(model, X, y):
+    """
+    Plots the decision boundary created by model predicting on X
+    :param model:
+    :param X:
+    :param y:
+    :return:
+    """
+    #define axis boundaries of the plot
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1  # add/subtract 0.1 to add a bit of margin
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+
+    # create meshgrid
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    # create X values to use tro make predictions
+    x_in = np.c_[xx.ravel(), yy.ravel()]  # stack 2d arrays
+    # print(xx.shape, yy.shape)
+    # print(xx, yy)
+    # print(x_in.shape)
+
+    # make predictions
+    y_pred = model.predict(x_in)
+
+    # check for multi-class
+    if len(y_pred[0]) > 1:
+        print("this is multiclass classification")
+        y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+    else:
+        print("this is binary classifiication")
+        y_pred = np.round(y_pred).reshape(xx.shape)
+
+    # plot the decision boundary
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    plt.show()
 
 
 def run(X, y):
@@ -64,3 +112,7 @@ def run(X, y):
                     metrics=["accuracy"])
     model_2.fit(X, y, epochs=100, verbose=0, workers=-1)
     print(model_2.evaluate(X, y))
+
+    # visualize predictions:
+    plot_decision_boundary(model_2, X, y)
+
