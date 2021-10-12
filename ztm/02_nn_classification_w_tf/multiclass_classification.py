ec359ea7f5137fefeec76ab51fa96171a3b00e71
jcisarano <cisarano@gmail.com>
2021-10-12 18:41:23 -0400

    plot images to visualize data

--- ztm/02_nn_classification_w_tf/multiclass_classification.py
+++ ztm/02_nn_classification_w_tf/multiclass_classification.py
@@ -2,7 +2,7 @@
 # NN that classifies different articles of clothing
 # using tf fashion_mnist dataset
 # 10 classes, 60k examples, test set 10k, 28x28 image
-
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.datasets import fashion_mnist
 
@@ -11,9 +11,23 @@ def load_data():
     # data is already sorted into training and test sets
     return fashion_mnist.load_data()
 
-def plot_one_data_sample(example):
+
+def plot_one_data_sample(example, name):
     import matplotlib.pyplot as plt
-    plt.imshow(example)
+    plt.imshow(example, cmap=plt.cm.binary)
+    plt.title(name)
+    plt.show()
+
+
+def plot_multiple_random_samples(t_data, t_labels, c_names):
+    import random
+    plt.figure(figsize=(7, 7))
+    for i in range(4):
+        ax = plt.subplot(2, 2, i+1)
+        rand_index = random.choice(range(len(t_data)))
+        plt.imshow(t_data[rand_index], cmap=plt.cm.binary)
+        plt.title(c_names[t_labels[rand_index]])
+        plt.axis(False)
     plt.show()
 
 
@@ -22,4 +36,12 @@ def run():
     # print(f"Training sample:\n {train_data[0]}")
     # print(f"Training label:\n {train_labels[0]}")
     # plot_one_data_sample(train_data[7])
-    
+
+    class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+                   "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot", ]
+
+    index = 440
+    plot_one_data_sample(train_data[index], class_names[train_labels[index]])
+
+    # plot multiple random images to help visualize data
+    plot_multiple_random_samples(t_data=train_data, t_labels=train_labels, c_names=class_names)
