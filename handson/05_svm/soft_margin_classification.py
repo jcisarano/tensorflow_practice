0ea5a3b1d9ceeaacef77ba6a2a7bb0ded6b93448
jcisarano <cisarano@gmail.com>
2021-10-17 16:18:16 -0400

    plot different soft margin examples, experiment with values of C

--- handson/05_svm/soft_margin_classification.py
+++ handson/05_svm/soft_margin_classification.py
@@ -63,7 +63,72 @@ def plot_hard_sensitivity_examples(X, y):
     plt.show()
 
 
-#def plot_large_margin_vs_fewer_margin_violations():
+def plot_large_margin_vs_fewer_margin_violations():
+    """
+    Examples of how different values for C change margins and number of margin violations.
+    Plot on left has low value of C, with wider margins and more violations
+    Plot on right has higher value of C, with narrower margins and fewer violations.
+    However, the plot on the left will probably generalize better
+    :return:
+    """
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.svm import LinearSVC
+
+    X, y = utils.load_iris_data_width_length()
+    scaler = StandardScaler()
+    svm_clf1 = LinearSVC(C=1, loss="hinge", random_state=42)
+    svm_clf2 = LinearSVC(C=100, loss="hinge", random_state=42)
+
+    scaled_svm_clf1 = Pipeline([
+        ("scaler", scaler),
+        ("linear_svc", svm_clf1),
+    ])
+    scaled_svm_clf2 = Pipeline([
+        ("scaler", scaler),
+        ("linear_svc", svm_clf2)
+    ])
+
+    scaled_svm_clf1.fit(X, y)
+    scaled_svm_clf2.fit(X, y)
+
+    # convert to unscaled params
+    b1 = svm_clf1.decision_function([-scaler.mean_ / scaler.scale_])
+    b2 = svm_clf2.decision_function([-scaler.mean_ / scaler.scale_])
+    w1 = svm_clf1.coef_[0] / scaler.scale_
+    w2 = svm_clf2.coef_[0] / scaler.scale_
+    svm_clf1.intercept_ = np.array([b1])
+    svm_clf2.intercept_ = np.array([b2])
+    svm_clf1.coef_ = np.array([w1])
+    svm_clf2.coef_ = np.array([w2])
+
+    # find support vectors (LinearSVC does not do this automatically)
+    t = y*2 - 1
+    support_vectors_idx1 = (t*(X.dot(w1) + b1) < 1).ravel()
+    support_vectors_idx2 = (t*(X.dot(w2) + b2) < 1).ravel()
+    svm_clf1.support_vectors_ = X[support_vectors_idx1]
+    svm_clf2.support_vectors_ = X[support_vectors_idx2]
+
+    fig, axes = plt.subplots(ncols=2, figsize=(10, 2.7), sharey=True)
+    plt.sca(axes[0])
+    plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "g^", label="Iris virginica")
+    plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "bs", label="Iris versicolor")
+    utils.plot_svc_decision_boundary(svm_clf=svm_clf1, xmin=4, xmax=5.9)
+    plt.xlabel("Petal length", fontsize=14)
+    plt.ylabel("Petal width", fontsize=14)
+    plt.legend(loc="upper left", fontsize=14)
+    plt.title("$C = {}$".format(svm_clf1.C), fontsize=16)
+    plt.axis([4, 5.9, 0.8, 2.8])
+
+    plt.sca(axes[1])
+    plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "g^")
+    plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "bs")
+    utils.plot_svc_decision_boundary(svm_clf=svm_clf2, xmin=4, xmax=5.99)
+    plt.xlabel("Petal length", fontsize=14)
+    plt.title("$C = {}$".format(svm_clf2.C), fontsize=16)
+    plt.axis([4, 5.9, 0.8, 2.8])
+    plt.show()
+
+
 
 
 
@@ -102,3 +167,6 @@ def run():
 
     soft_marg_svc_clf = soft_margin_svc_example()
     print(soft_marg_svc_clf.predict([[5.5, 1.7]]))
+
+    plot_large_margin_vs_fewer_margin_violations()
+
