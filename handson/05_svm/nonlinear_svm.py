ebf75b17285f53785582c094710151e25c791c63
jcisarano <cisarano@gmail.com>
2021-10-17 18:08:23 -0400

    kernel trick for polynomial features

--- handson/05_svm/nonlinear_svm.py
+++ handson/05_svm/nonlinear_svm.py
@@ -67,10 +67,51 @@ def nonlinear_svm_w_polynomial_features(X, y):
     return clf
 
 
+def nonlinear_svm_w_polynomial_kernel(X, y, coef0=1, degree=3):
+    """
+    Example of kernel trick. Uses polynomial kernel, which can be faster than adding high-degree polynomials
+    It doesn't actually add polynomials? just mimics the same effect???
+    degree var controls number of polynomials
+    coef0 determines how much the model is influenced by high-degree polynomials
+    :param X:
+    :param y:
+    :param coef0:
+    :param degree:
+    :return:
+    """
+    from sklearn.svm import SVC
+    clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=degree, coef0=coef0, C=5))
+    ])
+    clf.fit(X, y)
+    return clf
+
+
+def plot_poly_kernel_clfs(poly_clf, poly100_clf, X, y):
+    fig, axes = plt.subplots(ncols=2, figsize=(10.5, 4.5), sharey=True)
+    plt.sca(axes[0])
+    utils.plot_predictions(clf=poly_clf, axes=[-1.5, 2.45, -1, 1.5], show=False)
+    utils.plot_dataset(X=X, y=y, axes=[-1.5, 2.4, -1, 1.5], show=False)
+    plt.title(r"$d=3, r=1, c=5$", fontsize=18)
+
+    plt.sca(axes[1])
+    utils.plot_predictions(clf=poly100_clf, axes=[-1.5, 2.45, -1, 1.5], show=False)
+    utils.plot_dataset(X=X, y=y, axes=[-1.5, 2.4, -1, 1.5], show=False)
+    plt.title(r"$d=10, r=100, C=5$", fontsize=18)
+    plt.ylabel("")
+    plt.show()
+
+
 def run():
-    adding_features_to_dataset()
+    # adding_features_to_dataset()
     X, y = get_and_plot_moon_dataset()
-    polynomial_clf = nonlinear_svm_w_polynomial_features(X, y)
-    utils.plot_predictions(clf=polynomial_clf, axes=[-1.5, 2.5, -1, 1.5], show=False)
-    utils.plot_dataset(X=X, y=y, axes=[-1.5, 2.5, -1, 1.5])
+    # polynomial_clf = nonlinear_svm_w_polynomial_features(X, y)
+    # utils.plot_predictions(clf=polynomial_clf, axes=[-1.5, 2.5, -1, 1.5], show=False)
+    # utils.plot_dataset(X=X, y=y, axes=[-1.5, 2.5, -1, 1.5])
+
+    poly_kernel_svm_clf = nonlinear_svm_w_polynomial_kernel(X=X, y=y)
+    poly100_kernel_svm_clf = nonlinear_svm_w_polynomial_kernel(X=X, y=y, coef0=100, degree=10)
+    plot_poly_kernel_clfs(poly_clf=poly_kernel_svm_clf, poly100_clf=poly100_kernel_svm_clf, X=X, y=y)
+
 
