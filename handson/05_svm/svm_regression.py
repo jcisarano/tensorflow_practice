42ae03f129d4e2db56b10f3593b59b6d88933c35
jcisarano <cisarano@gmail.com>
2021-10-19 17:14:19 -0400

    added nonlinear regression with svm & plot

--- handson/05_svm/svm_regression.py
+++ handson/05_svm/svm_regression.py
@@ -33,6 +33,32 @@ def plot_svm_regression(svm_reg, X, y, axes):
     plt.axis(axes)
 
 
+def nonlinear_svm_regression():
+    np.random.seed(42)
+    m = 100
+    X = 2*np.random.rand(m, 1) - 1
+    y = (0.2 + 0.1*X + 0.5*X**2 + np.random.randn(m, 1)/10).ravel()
+
+    svm_poly_reg = SVR(kernel="poly", degree=2, C=100, epsilon=0.1, gamma="scale")
+    svm_poly_reg.fit(X, y)
+    print(svm_poly_reg)
+
+    svm_poly_reg1 = SVR(kernel="poly", degree=2, C=100, epsilon=0.1, gamma="scale")
+    svm_poly_reg2 = SVR(kernel="poly", degree=2, C=0.01, epsilon=0.1, gamma="scale")
+    svm_poly_reg1.fit(X, y)
+    svm_poly_reg2.fit(X, y)
+
+    fig, axes = plt.subplots(ncols=2, figsize=(9, 4), sharey=True)
+    plt.sca(axes[0])  # sets axes to param, sets current fig to parent of axes
+    plot_svm_regression(svm_poly_reg1, X, y, [-1, 1, 0, 1])
+    plt.title(r"$degree={}, C={}, \epsilon = {}$".format(svm_poly_reg1.degree, svm_poly_reg1.C, svm_poly_reg1.epsilon), fontsize=18)
+    plt.ylabel(r"$y$", fontsize=18, rotation=0)
+    plt.sca(axes[1])
+    plot_svm_regression(svm_poly_reg2, X, y, [-1, 1, 0, 1])
+    plt.title(r"$degree={}, C={}, \epsilon = {}$".format(svm_poly_reg2.degree, svm_poly_reg2.C, svm_poly_reg2.epsilon), fontsize=18)
+    plt.show()
+
+
 def do_svm_regression(X, y):
     """
     svm regression using kernel trick on linear data
@@ -65,6 +91,8 @@ def do_svm_regression(X, y):
 
 
 
+
+
 def run():
     np.random.seed(42)
     m = 50
@@ -76,4 +104,5 @@ def run():
     # model_svmr = create_linear_svr(X, y)
     # print(model_svmr)
 
-    do_svm_regression(X, y)
+    # do_svm_regression(X, y)
+    nonlinear_svm_regression()
