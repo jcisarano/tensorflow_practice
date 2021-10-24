52a5623ad7822620f7dcc932e2451b0252b95d5a
jcisarano <cisarano@gmail.com>
2021-10-24 09:14:07 -0400

    visualize tree using graphviz

--- handson/06_decision_trees/basic_decision_tree_example.py
+++ handson/06_decision_trees/basic_decision_tree_example.py
@@ -1,5 +1,9 @@
+import os
+
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
+from graphviz import Source
+from sklearn.tree import export_graphviz
 
 
 def run():
@@ -12,3 +16,14 @@ def run():
     tree_clf.fit(X, y)
 
     print(tree_clf)
+
+    export_graphviz(
+        tree_clf,
+        out_file=os.path.join("./images/", "iris_tree.dot"),
+        feature_names=iris.feature_names[2:],
+        rounded=True,
+        filled=True
+    )
+    graph = Source.from_file(os.path.join("./images/", "iris_tree.dot"))
+    graph.format = "png"
+    graph.render("iris_tree_render", view=True)
