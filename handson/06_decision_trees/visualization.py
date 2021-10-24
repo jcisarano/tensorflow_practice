b13b22d9dec472e9de5a93cfaa03013014666b27
jcisarano <cisarano@gmail.com>
2021-10-24 08:56:37 -0400

    started visualization library

---
+++ handson/06_decision_trees/visualization.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
+def plot_decision_boundary(clf, X, y, axes=[0, 7.5, 0, 3], iris=True, legend=False, plot_training=True):
+    x1s = np.linspace(axes[0], axes[1], 100)
+    x2s = np.linspace(axes[2], axes[3], 100)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    X_new = np.c_[x1.ravel(), x2.ravel()]
+    y_pred = clf.predict(X_new).reshape(x1.shape)
+    custom_cmap = ListedColormap(["#fafab0", "#9898ff", "#a0faa0"])
+    plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=custom_cmap)
+    if not iris:
+        custom_cmap2 = ListedColormap(["#7d7d58", "#4c4c7f", "#507d50"])
+        plt.contour(x1, x2, y_pred, cmap=custom_cmap2, alpha=0.8)
+    if plot_training:
+        plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "yo", label="Iris setosa")
+        plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "yo", label="Iris versicolor")
+        plt.plot(X[:, 0][y == 2], X[:, 1][y == 2], "yo", label="Iris virginica")
+    if iris:
+        plt.xlabel("Petal width", fontsize=14)
+        plt.ylabel("Petal height", fontsize=14)
+    else:
+        plt.xlabel("$x_1$", fontsize=18)
+        plt.ylabel("$x_2$", fontsize=18)
+    if legend:
+        plt.legend(loc="lower right", fontsize=14)
