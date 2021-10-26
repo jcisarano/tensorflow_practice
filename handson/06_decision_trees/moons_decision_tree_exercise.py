12325489db62a244984eebae710a51eb54e0e8a5
jcisarano <cisarano@gmail.com>
2021-10-26 08:55:18 -0400

    start random forest exercise

--- handson/06_decision_trees/moons_decision_tree_exercise.py
+++ handson/06_decision_trees/moons_decision_tree_exercise.py
@@ -1,12 +1,8 @@
 import numpy as np
 from sklearn.datasets import make_moons
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, ShuffleSplit
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error, accuracy_score
-from scipy.stats import reciprocal, uniform
-
-import visualization as vv
-import matplotlib.pyplot as plt
 
 
 def run():
@@ -24,9 +20,22 @@ def run():
     params = {"max_leaf_nodes": list(range(2, 200)), "min_samples_split": [2, 3, 4]}
     grid_search = GridSearchCV(tree_grid_search, params, verbose=2, cv=3, n_jobs=-1)
     grid_search.fit(X_train, y_train)
-    print(grid_search.best_estimator_)
+    print(grid_search.best_estimator_, grid_search.best_estimator_.__getattribute__("min_samples_split"))
 
     y_pred = grid_search.best_estimator_.predict(X_test)
     print(accuracy_score(y_test, y_pred))
 
+    max_leaf_nodes = grid_search.best_estimator_.__getattribute__("max_leaf_nodes")
+    min_samples_split = grid_search.best_estimator_.__getattribute__("min_samples_split")
 
+    # Grow a forest
+    # data set has 10000 items, so create train set of 100 items with 20% test set
+    split = ShuffleSplit(n_splits=1000, train_size=0.0125, test_size=0.0025, random_state=42)
+
+    for train_index, test_index in split.split(X_train):
+        print("TRAIN", len(train_index), "TEST", len(test_index))
+        # tree = DecisionTreeClassifier(max_leaf_nodes=max_leaf_nodes,
+        #                               min_samples_split=min_samples_split,
+        #                               random_state=42)
+        # tree.fit(X[train_index], y[train_index])
+        # tree.predict(y)
