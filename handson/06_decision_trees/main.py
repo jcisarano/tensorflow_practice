4a637fc73e0933a50bd26c0f174c2737d69fe3e6
jcisarano <cisarano@gmail.com>
2021-10-26 07:46:32 -0400

    start moon decision tree exercise

--- handson/06_decision_trees/main.py
+++ handson/06_decision_trees/main.py
@@ -3,11 +3,13 @@
 import basic_decision_tree_example as bdte
 import tree_with_regularization as twr
 import regression_example as re
+import moons_decision_tree_exercise as mdt
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # bdte.run()
     # twr.run()
-    re.run()
+    # re.run()
+    mdt.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
