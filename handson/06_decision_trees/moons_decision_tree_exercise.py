5c92cb462120207258fcabb4f26dca1fb3bacc56
jcisarano <cisarano@gmail.com>
2021-10-26 18:02:33 -0400

    added book solution

--- handson/06_decision_trees/moons_decision_tree_exercise.py
+++ handson/06_decision_trees/moons_decision_tree_exercise.py
@@ -3,6 +3,7 @@ from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split, GridSearchCV, ShuffleSplit
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.base import clone
 
 from scipy.stats import mode
 
@@ -56,4 +57,39 @@ def run():
     # print(y_pred.ravel().shape)
     print("Forest accuracy: ", accuracy_score(y_test, y_pred))
     # print(mode(pred, axis=1))
+
+
+    # Forest book solution:
+    n_trees = 1000
+    n_instances = 100
+    mini_sets = []
+
+    # creates train size by subtracting desired len from full set size and using what's left
+    # i.e. test_size=8000-100, so test_size = total-test_size). Test set is not used
+    rs = ShuffleSplit(n_splits=n_trees, test_size=len(X_train) - n_instances, random_state=42)
+    for mini_train_index, mini_test_index in rs.split(X_train):
+        X_mini_train = X_train[mini_train_index]
+        y_mini_train = y_train[mini_train_index]
+        # print(X_mini_train.shape, y_mini_train.shape)
+        mini_sets.append((X_mini_train, y_mini_train))
+
+    # loop to clone best estimator 1000 times
+    forest = [clone(grid_search.best_estimator_) for _ in range(n_trees)]
+    accuracy_scores = []
+
+    for tree, (X_mini_train, y_mini_train) in zip(forest, mini_sets):
+        tree.fit(X_mini_train, y_mini_train)
+
+        y_pred = tree.predict(X_test)
+        accuracy_scores.append(accuracy_score(y_test, y_pred))
+
+    print("Book mean accuracy:", np.mean(accuracy_scores))
+
+    Y_pred = np.empty([n_trees, len(X_test)], dtype=np.uint8)
+
+    for tree_index, tree in enumerate(forest):
+        Y_pred[tree_index] = tree.predict(X_test)
+
+    y_pred_majority_votes, n_votes = mode(Y_pred, axis=0)
+    print("Book forest accuracy:", accuracy_score(y_test, y_pred_majority_votes.reshape([-1])))
 
