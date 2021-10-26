1f9cb75f7f19baa06ad0e89c67b3d14b37508e98
jcisarano <cisarano@gmail.com>
2021-10-26 17:35:14 -0400

    finished forest classifier

--- handson/06_decision_trees/moons_decision_tree_exercise.py
+++ handson/06_decision_trees/moons_decision_tree_exercise.py
@@ -1,8 +1,10 @@
 import numpy as np
 from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split, GridSearchCV, ShuffleSplit
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+from scipy.stats import mode
 
 
 def run():
@@ -14,16 +16,16 @@ def run():
     tree_model.fit(X_train, y_train)
 
     y_pred = tree_model.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
+    print("Single classifier accuracy:", accuracy_score(y_test, y_pred))
 
     tree_grid_search = DecisionTreeClassifier(random_state=42)
     params = {"max_leaf_nodes": list(range(2, 200)), "min_samples_split": [2, 3, 4]}
-    grid_search = GridSearchCV(tree_grid_search, params, verbose=2, cv=3, n_jobs=-1)
+    grid_search = GridSearchCV(tree_grid_search, params, verbose=1, cv=3, n_jobs=-1)
     grid_search.fit(X_train, y_train)
-    print(grid_search.best_estimator_, grid_search.best_estimator_.__getattribute__("min_samples_split"))
+    # print(grid_search.best_estimator_, grid_search.best_estimator_.__getattribute__("min_samples_split"))
 
     y_pred = grid_search.best_estimator_.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
+    print("Best estimator accuracy:", accuracy_score(y_test, y_pred))
 
     max_leaf_nodes = grid_search.best_estimator_.__getattribute__("max_leaf_nodes")
     min_samples_split = grid_search.best_estimator_.__getattribute__("min_samples_split")
@@ -34,13 +36,24 @@ def run():
 
     forest = []
     for train_index, test_index in split.split(X_train):
-        print("TRAIN", len(train_index), "TEST", len(test_index))
         tree = DecisionTreeClassifier(max_leaf_nodes=max_leaf_nodes,
                                       min_samples_split=min_samples_split,
                                       random_state=42)
         tree.fit(X_train[train_index], y_train[train_index])
         forest.append(tree)
 
+    pred = None
     for tree in forest:
         y_pred = tree.predict(X_test)
-        print(accuracy_score(y_test, y_pred))
+        if pred is None:
+            pred = y_pred
+        else:
+            pred = np.c_[pred, y_pred]
+        # print(pred.shape)
+        # print("Accuracy: ", accuracy_score(y_test, y_pred), " ", y_pred[0], " ", X_test[0])
+
+    y_pred, counts = mode(pred, axis=1)
+    # print(y_pred.ravel().shape)
+    print("Forest accuracy: ", accuracy_score(y_test, y_pred))
+    # print(mode(pred, axis=1))
+
