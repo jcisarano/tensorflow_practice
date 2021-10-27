d99a707db3cd89e1f2dbbd7bb336c136bd747de7
jcisarano <cisarano@gmail.com>
2021-10-27 08:23:37 -0400

    added function to count saved files

--- ztm/03_convolutional_nn_w_tf/fetch_data.py
+++ ztm/03_convolutional_nn_w_tf/fetch_data.py
@@ -10,10 +10,11 @@ def fetch_remote_data(remote_data_url, local_save_path, local_filename):
     urllib.request.urlretrieve(remote_data_url, full_local_path)
     with zipfile.ZipFile(full_local_path, 'r') as zip_ref:
         zip_ref.extractall(local_save_path)
-    # loaded_file = tarfile.open(full_local_path)
-    # loaded_file.extractall(path=local_save_path)
-    # loaded_file.close()
 
+
+def examine_files(dir):
+    for dirpath, dirnames, filenames in os.walk(dir):
+        print(f"There are {len(dirnames)} directories and {len(filenames)} images in {dirpath}")
 
 def load_data(path, filename):
     csv_path = os.path.join(path, filename)
--- ztm/03_convolutional_nn_w_tf/food_vision.py
+++ ztm/03_convolutional_nn_w_tf/food_vision.py
@@ -2,10 +2,13 @@
 import os
 import fetch_data as fd
 
-DATA_PATH : str = "https://storage.googleapis.com/ztm_tf_course/food_vision/pizza_steak.zip"
-LOCAL_SAVE_PATH : str = os.path.join("datasets", "images")
+# images are from food 101 dataset, but reduced to include only images of pizza and steak for now
+# Starting with a smaller dataset allows quicker experimentation
+DATA_PATH: str = "https://storage.googleapis.com/ztm_tf_course/food_vision/pizza_steak.zip"
+LOCAL_SAVE_PATH: str = os.path.join("datasets", "images")
 LOCAL_FILE_NAME: str = "steak_pizza.zip"
 
 
 def run():
     # fd.fetch_remote_data(DATA_PATH, LOCAL_SAVE_PATH, LOCAL_FILE_NAME)
+    fd.examine_files(os.path.join(LOCAL_SAVE_PATH, "pizza_steak"))
