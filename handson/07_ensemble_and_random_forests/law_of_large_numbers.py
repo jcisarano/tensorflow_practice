295d8d4a364c2554e028f4139a48a3a0e1a86fc5
jcisarano <cisarano@gmail.com>
2021-11-04 18:17:03 -0400

    added explanatory notes

--- handson/07_ensemble_and_random_forests/law_of_large_numbers.py
+++ handson/07_ensemble_and_random_forests/law_of_large_numbers.py
@@ -4,15 +4,27 @@ import numpy as np
 
 def run():
     heads_probability = 0.51
+    # produce 10x10000 matrix to represent 10 coins tossed 10000 times
+    # coin_tosses.shape = (10000, 10)
     coin_tosses = (np.random.rand(10000, 10) < heads_probability).astype(np.int32)
+    # np.cumsum along axis 0 adds up cols of 0/1 vals
+    # np.arange returns evenly spaced values over range 1, 10001
+    # np.reshape makes array shape (10000, 1)
+    # so each row is divided by its index: row 1 divided by 1, row 2 divided by 2, etc
     cumulative_heads_ratio = np.cumsum(coin_tosses, axis=0) / np.arange(1, 10001).reshape(-1, 1)
 
     plt.figure(figsize=(8, 4.5))
+    # plots all the coin toss values
     plt.plot(cumulative_heads_ratio)
+
+    # draw the horizontal lines
     plt.plot([0, 10000], [0.51, 0.51], "k--", linewidth=2, label="51%")
     plt.plot([0, 10000], [0.5, 0.5], "k-", label="50%")
+
     plt.xlabel("Number of coin tosses")
     plt.ylabel("Heads ratio")
     plt.legend(loc="lower right")
+
+    # make the axes fit the data
     plt.axis([0, 10000, 0.42, 0.58])
     plt.show()
