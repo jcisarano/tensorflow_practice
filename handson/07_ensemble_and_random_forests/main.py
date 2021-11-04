bdc1f1f756b1ae9beb1478ca73f80521592e5c33
jcisarano <cisarano@gmail.com>
2021-11-04 17:55:47 -0400

    plot law of large numbers

---
+++ handson/07_ensemble_and_random_forests/main.py
@@ -0,0 +1,7 @@
+
+import law_of_large_numbers as law
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    law.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
