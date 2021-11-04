1f70211e83aafc5edf861a284e6f82279d7becd6
jcisarano <cisarano@gmail.com>
2021-11-04 18:33:52 -0400

    completed first simple hard voting classifier

--- handson/07_ensemble_and_random_forests/first_voting_classifier.py
+++ handson/07_ensemble_and_random_forests/first_voting_classifier.py
@@ -1,5 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_moons
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 
 def get_moons():
@@ -14,3 +19,24 @@ def get_moons():
 
 def run():
     X_train, X_test, y_train, y_test = get_moons()
+
+    # individual classifiers:
+    log_clf = LogisticRegression(solver="lbfgs", random_state=42)
+    rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    svm_clf = SVC(gamma="scale", random_state=42)
+
+    # voting classifier combines each of the individuals
+    # voting=hard makes this a hard classifier, the class with the most votes wins:
+    voting_clf = VotingClassifier(
+        estimators=[("lr", log_clf), ("rf", rnd_clf), ("svc", svm_clf)],
+        voting="hard"
+    )
+    # fits them all together:
+    voting_clf.fit(X_train, y_train)
+
+    # write the accuracy score of each individual followed by the combined voting classifier
+    # the voting classifier should win:
+    for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
