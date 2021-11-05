3394e14943bc49b988a27f22605c5612df127afd
jcisarano <cisarano@gmail.com>
2021-11-05 08:16:44 -0400

    plot bagging ensemble v single decision tree

--- handson/07_ensemble_and_random_forests/bagging_and_pasting.py
+++ handson/07_ensemble_and_random_forests/bagging_and_pasting.py
@@ -1,3 +1,6 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
 from sklearn.datasets import make_moons
 from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
@@ -6,9 +9,33 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 def get_moons():
-    X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
+    X, y = get_raw_moons()
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
     return X_train, X_test, y_train, y_test
+
+
+def get_raw_moons():
+    return make_moons(n_samples=500, noise=0.30, random_state=42)
+
+
+def plot_decision_boundary(clf, X, y, axes=[-1.5, 2.45, -1, 1.5], alpha=0.5, contour=True):
+    # np.linspace(x,y,z) creates z evenly spaced values over given range (x,y)
+    x1s = np.linspace(axes[0], axes[1], 100)
+    x2s = np.linspace(axes[2], axes[3], 100)
+    # np.meshgrid creates regular xy grid with all combinations of x1s, x2s
+    x1, x2 = np.meshgrid(x1s, x2s)
+    X_new = np.c_[x1.ravel(), x2.ravel()]
+    y_pred = clf.predict(X_new).reshape(x1.shape)
+    custom_cmap = ListedColormap(["#fafab0", "#9898ff", "#a0faa0"])
+    plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=custom_cmap)
+    if contour:
+        custom_cmap2 = ListedColormap(["#7d7d58", "#4c4c7f", "#507d50"])
+        plt.contour(x1, x2, y_pred, cmap=custom_cmap2, alpha=0.8)
+    plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "yo", alpha=alpha)
+    plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "bs", alpha=alpha)
+    plt.axis(axes)
+    plt.xlabel(r"$x_1$", fontsize=18)
+    plt.ylabel(r"$x_2$", fontsize=18, rotation=0)
 
 
 def plot_decision_tree_v_bagging(X_train, X_test, y_train, y_test):
@@ -25,10 +52,18 @@ def plot_decision_tree_v_bagging(X_train, X_test, y_train, y_test):
     y_pred_tree = tree_clf.predict(X_test)
     print("DecisionTreeClassifier accuracy:", accuracy_score(y_test, y_pred_tree))
 
-
+    X, y = get_raw_moons()
+    fix, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+    plt.sca(axes[0])
+    plot_decision_boundary(tree_clf, X, y)
+    plt.title("Decision Tree", fontsize=14)
+    plt.sca(axes[1])
+    plot_decision_boundary(bag_clf, X, y)
+    plt.title("Decision Trees with Bagging", fontsize=14)
+    plt.ylabel("")
+    plt.show()
 
 
 def run():
     X_train, X_test, y_train, y_test = get_moons()
     plot_decision_tree_v_bagging(X_train, X_test, y_train, y_test)
-
