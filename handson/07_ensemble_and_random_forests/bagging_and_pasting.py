4e3df26a77f2c6ec901fe130b61eaded20ce179b
jcisarano <cisarano@gmail.com>
2021-11-05 08:24:49 -0400

    added notes

--- handson/07_ensemble_and_random_forests/bagging_and_pasting.py
+++ handson/07_ensemble_and_random_forests/bagging_and_pasting.py
@@ -23,16 +23,25 @@ def plot_decision_boundary(clf, X, y, axes=[-1.5, 2.45, -1, 1.5], alpha=0.5, con
     x1s = np.linspace(axes[0], axes[1], 100)
     x2s = np.linspace(axes[2], axes[3], 100)
     # np.meshgrid creates regular xy grid with all combinations of x1s, x2s
+    # here shape is (100,100)
     x1, x2 = np.meshgrid(x1s, x2s)
     X_new = np.c_[x1.ravel(), x2.ravel()]
+
+    # make predictions based on regular data generated above
     y_pred = clf.predict(X_new).reshape(x1.shape)
     custom_cmap = ListedColormap(["#fafab0", "#9898ff", "#a0faa0"])
+    # plt.contourf fills background color, plt.contour does not
     plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=custom_cmap)
+
+    # draws a heavier line over the prediction boundary for contrast
     if contour:
         custom_cmap2 = ListedColormap(["#7d7d58", "#4c4c7f", "#507d50"])
         plt.contour(x1, x2, y_pred, cmap=custom_cmap2, alpha=0.8)
+
+    # plots original data for reference
     plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "yo", alpha=alpha)
     plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "bs", alpha=alpha)
+
     plt.axis(axes)
     plt.xlabel(r"$x_1$", fontsize=18)
     plt.ylabel(r"$x_2$", fontsize=18, rotation=0)
