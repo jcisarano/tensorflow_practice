3370bc03b968bda5fec6e5417c9b154f5849cc55
jcisarano <cisarano@gmail.com>
2021-11-07 09:19:25 -0500

    working on plotting gradient boosting

--- handson/07_ensemble_and_random_forests/gradient_boosting.py
+++ handson/07_ensemble_and_random_forests/gradient_boosting.py
@@ -17,9 +17,9 @@ def create_quadratic_dataset():
 
 def plot_predictions(regressors, X, y, axes, label=None, style="r-", data_style="b.", data_label=None):
     x1 = np.linspace(axes[0], axes[1], 500)
-    y_pred = sum(regressor.predict(x1.reshape(-1,1)) for regressor in regressors)
+    y_pred = sum(regressor.predict(x1.reshape(-1, 1)) for regressor in regressors)
     plt.plot(X[:, 0], y, data_style, label=data_label)
-    plt.plot(x1, y_pred, style, linewidth=2, label=2)
+    plt.plot(x1, y_pred, style, linewidth=2, label=label)
     if label or data_label:
         plt.legend(loc="upper center", fontsize=16)
     plt.axis(axes)
@@ -41,6 +41,32 @@ def simple_gradient_boost_eg(X, y):
     y_pred = sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
     print(y_pred)
 
+    # plot
+    plt.figure(figsize=(11, 11))
+
+    # plot residuals and tree predictions
+    plt.subplot(321)
+    plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h_1(x_1)$", style="g-", data_label="Training set")
+    plt.ylabel("$y$", fontsize=16, rotation=0)
+    plt.title("Residuals and tree predictions", fontsize=16)
+
+    # plot ensemble predictions, using only first tree
+    plt.subplot(322)
+    plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1)$", data_label="Training set")
+    plt.ylabel("$y$", fontsize=16, rotation=0)
+    plt.title("Ensemble predictions", fontsize=16)
+
+    # plot second tree, which was trained on results of first tree
+    plt.subplot(323)
+    plot_predictions([tree_reg2], X, y2, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_2(x_1)$", style="g-", data_style="k+", data_label="Residuals")
+    plt.ylabel("$y - h_1(x_1)$", fontsize=16)
+
+    # plot ensemble predictions using first and second tree
+    plt.subplot(324)
+    plot_predictions([tree_reg1, tree_reg2], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1) + h_2(x_1)$")
+    plt.ylabel("$y$", fontsize=16, rotation=0)
+
+    plt.show()
 
 def run():
     X, y = create_quadratic_dataset()
