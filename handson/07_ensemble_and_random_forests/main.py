3568f409b5fb559b37ef07e8a15273c1f14b7afb
jcisarano <cisarano@gmail.com>
2021-11-08 08:20:48 -0500

    started mnist exercise, fetch data

--- handson/07_ensemble_and_random_forests/main.py
+++ handson/07_ensemble_and_random_forests/main.py
@@ -5,10 +5,12 @@ import bagging_and_pasting as bp
 import random_forests as rf
 import ada_boost_example as abe
 import  gradient_boosting as gb
+import mnist_exercise as mne
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    gb.run()
+    mne.run()
+    # gb.run()
     # abe.run()
     # rf.run()
     # bp.run()
---
+++ handson/07_ensemble_and_random_forests/mnist_exercise.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn.datasets import fetch_openml
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+
+
+def fetch_mnist_data(size=70000):
+    mnist = fetch_openml('mnist_784', version=1, cache=True, as_frame=False)
+    mnist.target = mnist.target.astype(np.int8)  # converts from string to int
+    X, y = mnist["data"], mnist["target"]
+    if size < len(X):
+        X = X[:size]
+        y = y[:size]
+    return X, y
+
+
+def train_val_test_split(X, y, train_size=0.714, test_size=0.143, validation_size=0.143):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1-train_size, random_state=42)
+    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test,
+                                                    test_size=test_size/(test_size+validation_size),
+                                                    shuffle=False)
+    return X_train, X_test, X_val, y_train, y_test, y_val
+
+
+def run():
+    X, y = fetch_mnist_data()
+    X_train, X_test, X_val, y_train, y_test, y_val = train_val_test_split(X, y)
+    print(X_train.shape, X_test.shape, X_val.shape)
+
