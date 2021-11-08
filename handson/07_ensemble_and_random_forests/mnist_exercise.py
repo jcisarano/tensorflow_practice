aceb6351204e9db1b40ac17944089b464579999d
jcisarano <cisarano@gmail.com>
2021-11-08 08:21:18 -0500

    added classifier functions

--- handson/07_ensemble_and_random_forests/mnist_exercise.py
+++ handson/07_ensemble_and_random_forests/mnist_exercise.py
@@ -16,6 +16,28 @@ def fetch_mnist_data(size=70000):
     return X, y
 
 
+def train_random_forest(X_train, y_train, X_test):
+    clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, random_state=42)
+    clf.fit(X_train, y_train)
+    # y_pred_rf = clf.predict(X_test)
+
+    return clf
+
+
+def train_svm(X_train, y_train, X_test):
+    clf = SVC()
+    clf.fit(X_train, y_train)
+
+    return clf
+
+
+def train_extra_trees(X_train, y_train, X_test):
+    clf = ExtraTreesClassifier()
+    clf.fit(X_train, y_train)
+
+    return clf
+
+
 def train_val_test_split(X, y, train_size=0.714, test_size=0.143, validation_size=0.143):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1-train_size, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_test, y_test,
