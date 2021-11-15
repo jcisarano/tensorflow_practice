4fe501066b8d028b48f4ec6aa8f17d547adb9f1a
jcisarano <cisarano@gmail.com>
2021-11-15 18:25:56 -0500

    now try softmax regression

--- handson/08_dimensionality_reduction/pca_timing_exercise.py
+++ handson/08_dimensionality_reduction/pca_timing_exercise.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import fetch_openml
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -21,14 +22,14 @@ def run():
     X_train, X_test, y_train, y_test = get_mnist()
 
     print(X_train.shape, X_test.shape)
-    rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
-    t1 = time.time()
-    rnd_clf.fit(X_train, y_train)
-    t2 = time.time()
-    y_pred_rf = rnd_clf.predict(X_test)
-    print("Random forest score:", rnd_clf.score(X_test, y_test))
-    print("Random forest accuracy score:", accuracy_score(y_test, y_pred_rf))
-    print("Random forest time:", t2 - t1)
+    # rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    # t1 = time.time()
+    # rnd_clf.fit(X_train, y_train)
+    # t2 = time.time()
+    # y_pred_rf = rnd_clf.predict(X_test)
+    # print("Random forest score:", rnd_clf.score(X_test, y_test))
+    # print("Random forest accuracy score:", accuracy_score(y_test, y_pred_rf))
+    # print("Random forest time:", t2 - t1)
 
     pca = PCA(n_components=0.95)
     X_train_reduced = pca.fit_transform(X_train)
@@ -45,7 +46,17 @@ def run():
     # print("RF PCA accuracy:", accuracy_score(y_test, y_pred_rf_pca))
     # print("RF PCA time:", t2 - t1)
 
-    
+    # try softmax regression
+    log_clf = LogisticRegression(multi_class="multinomial", solver="lbfgs", random_state=42)
+    t1 = time.time()
+    log_clf.fit(X_train, y_train)
+    t2 = time.time()
+    y_pred_lr = log_clf.predict(X_test)
+
+    print("LR score:", log_clf.score(X_test, y_test))
+    print("LR accuracy score:", accuracy_score(y_test, y_pred_lr))
+    print("LR training time:", t2 - t1)
+
 
 
 
