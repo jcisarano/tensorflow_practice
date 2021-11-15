d0cad8c880b298fbba9f2ebc4a8629f3ca28bb02
jcisarano <cisarano@gmail.com>
2021-11-15 07:31:59 -0500

    added lda example

--- handson/08_dimensionality_reduction/other_examples.py
+++ handson/08_dimensionality_reduction/other_examples.py
@@ -1,11 +1,23 @@
-from sklearn.datasets import make_swiss_roll
+import numpy as np
+from sklearn.datasets import make_swiss_roll, fetch_openml
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.manifold import MDS, Isomap, TSNE
+from sklearn.model_selection import train_test_split
 
 
 def get_data(n_samples=1000, noise=0.2):
     X, t = make_swiss_roll(n_samples=n_samples, noise=noise, random_state=41)
     return X, t
+
+def get_mnist():
+    mnist = fetch_openml("mnist_784", version=1, as_frame=False)
+    mnist.target = mnist.target.astype(np.uint8)
+
+    X = mnist["data"]
+    y = mnist["target"]
+
+    return X, y
+    # return train_test_split(X, y)
 
 def run():
     X, t = get_data()
@@ -19,4 +31,10 @@ def run():
     tsne = TSNE(n_components=2, random_state=42)
     X_reduced_tsne = tsne.fit_transform(X)
 
-    # lda = LinearDiscriminantAnalysis(n_components=2)
+    lda = LinearDiscriminantAnalysis(n_components=2)
+    X_mnist, y_mnist = get_mnist()
+    lda.fit(X_mnist, y_mnist)
+    X_reduced_lda = lda.transform(X_mnist)
+
+
+    
