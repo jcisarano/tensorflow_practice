43eae5fb2d052099af604fd984b29ab28ae383a4
jcisarano <cisarano@gmail.com>
2021-11-15 17:59:47 -0500

    train random forest classifier

--- handson/08_dimensionality_reduction/pca_timing_exercise.py
+++ handson/08_dimensionality_reduction/pca_timing_exercise.py
@@ -1,6 +1,9 @@
 from sklearn.datasets import fetch_openml
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
+import time
 
 
 def get_mnist():
@@ -13,9 +16,14 @@ def get_mnist():
     return train_test_split(X, y, train_size=60000)
 
 
-
 def run():
     X_train, X_test, y_train, y_test = get_mnist()
 
     print(X_train.shape, X_test.shape)
-    
+    rnd_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    t1 = time.time()
+    rnd_clf.fit(X_train, y_train)
+    t2 = time.time()
+    y_pred_rf = rnd_clf.predict(X_test)
+    print("Random forest score:", rnd_clf.score(X_test, y_test))
+    print("Random forest time:", t2 - t1)
