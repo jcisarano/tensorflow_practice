487a975e9d12bb56566f0de34759713718376371
jcisarano <cisarano@gmail.com>
2021-11-15 07:38:19 -0500

    plot various examples

--- handson/08_dimensionality_reduction/other_examples.py
+++ handson/08_dimensionality_reduction/other_examples.py
@@ -3,6 +3,7 @@ from sklearn.datasets import make_swiss_roll, fetch_openml
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.manifold import MDS, Isomap, TSNE
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 def get_data(n_samples=1000, noise=0.2):
@@ -36,5 +37,17 @@ def run():
     lda.fit(X_mnist, y_mnist)
     X_reduced_lda = lda.transform(X_mnist)
 
+    titles = ["MDS", "Isomap", "t-SNE"]
 
-    
+    plt.figure(figsize=(11, 4))
+
+    for subplot, title, X_reduced in zip((131, 132, 133), titles, (X_reduced_mds, X_reduced_isomap, X_reduced_tsne)):
+        plt.subplot(subplot)
+        plt.title(title, fontsize=14)
+        plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=t, cmap=plt.cm.hot)
+        plt.xlabel("$z_1$", fontsize=18)
+        if subplot == 131:
+            plt.ylabel("$z_2$", fontsize=18, rotation=0)
+        plt.grid(True)
+
+    plt.show()
