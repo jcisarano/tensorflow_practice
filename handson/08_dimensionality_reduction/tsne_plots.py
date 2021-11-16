c16b5363e84d3857b89dd45e533421bf7019ec32
jcisarano <cisarano@gmail.com>
2021-11-16 08:23:58 -0500

    firstr plot tsne

--- handson/08_dimensionality_reduction/tsne_plots.py
+++ handson/08_dimensionality_reduction/tsne_plots.py
@@ -1,9 +1,13 @@
 from sklearn.datasets import fetch_openml
 import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
 
 def get_mnist(m=10000):
-    mnist = fetch_openml("mnist_784", version=1, as_frame=False)
-
+    mnist = fetch_openml("mnist_784", version=1, as_frame=False, cache=True)
+    mnist.target = mnist.target.astype(np.uint8)
+    
     idx = np.random.permutation(60000)[:m]
 
     X = mnist["data"][idx]
@@ -12,5 +16,23 @@ def get_mnist(m=10000):
     return X, y
 
 
+def scatter_plot(X_reduced, y):
+    plt.figure(figsize=(10, 7))
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y, cmap="jet")
+    plt.colorbar()
+    plt.axis("off")
+    plt.show()
+
+
 def run():
     X, y = get_mnist()
+    # classes = np.unique(y)
+
+    tsne = TSNE(n_components=2, init="random")
+    X_reduced = tsne.fit_transform(X)
+
+    norm = plt.Normalize(vmin=0, vmax=9)
+    c = norm(y)
+    scatter_plot(X_reduced, c)
+
+
