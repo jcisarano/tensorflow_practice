ebcd71b40f7550ea8d806af3618e0756b32737b4
jcisarano <cisarano@gmail.com>
2021-11-16 08:59:20 -0500

    start plot digits function

--- handson/08_dimensionality_reduction/tsne_plots.py
+++ handson/08_dimensionality_reduction/tsne_plots.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_openml
 import numpy as np
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 
 def get_mnist(m=10000):
@@ -46,6 +47,20 @@ def improve_2_3_5(X_reduced, X, y):
     X_subset_reduced = tsne_subset.fit_transform(X_subset)
     plot_subset(X_subset_reduced, y_subset)
 
+
+def plot_digits(X, y, min_distance=0.05, images=None, figsize=(13, 10)):
+    X_normalized = MinMaxScaler().fit_transform(X)
+    neighbors = np.array([[10., 10.]])
+    plt.figure(figsize=figsize)
+    cmap = plt.cm.get_cmap("jet")
+    digits = np.unique(y)
+    for digit in digits:
+        plt.scatter(X_normalized[y == digit, 0], X_normalized[y == digit, 1], c=[cmap(digit / 9)])
+    plt.axis("off")
+
+    plt.show()
+
+
 def run():
     X, y = get_mnist()
     # classes = np.unique(y)
@@ -53,9 +68,11 @@ def run():
     tsne = TSNE(n_components=2, learning_rate="auto", random_state=42)
     X_reduced = tsne.fit_transform(X)
 
-    norm = plt.Normalize(vmin=0, vmax=9)
-    c = norm(y)
-    scatter_plot(X_reduced, c)
+    # norm = plt.Normalize(vmin=0, vmax=9)
+    # c = norm(y)
+    # scatter_plot(X_reduced, c)
 
-    improve_2_3_5(X_reduced, X, y)
+    # improve_2_3_5(X_reduced, X, y)
+
+    plot_digits(X_reduced, y)
 
