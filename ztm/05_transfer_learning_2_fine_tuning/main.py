559b41f4340c35118d3d6d3306146ac51072e974
jcisarano <cisarano@gmail.com>
2021-11-17 07:55:38 -0500

    started fine tuning project w helper functions

---
+++ ztm/05_transfer_learning_2_fine_tuning/main.py
@@ -0,0 +1,9 @@
+# Transfer learning with TensorFlow part 2: Fine Tuning
+#
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
