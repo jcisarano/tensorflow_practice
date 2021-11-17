d947d68e879d70b16effdc1b022376fd7b60cf5d
jcisarano <cisarano@gmail.com>
2021-11-17 18:00:54 -0500

    load data batches

--- ztm/05_transfer_learning_2_fine_tuning/food_classifier.py
+++ ztm/05_transfer_learning_2_fine_tuning/food_classifier.py
@@ -1,6 +1,7 @@
 # food classifier using pretrained model from tf.keras.application
 # https://www.tensorflow.org/api_docs/python/tf/keras/applications
 import os
+import tensorflow as tf
 
 from helper_functions import create_tensorboard_callback, plot_loss_curves, unzip_data, walk_through_dir
 
@@ -13,7 +14,20 @@ IMG_SIZE: int = 224
 IMG_SHAPE = (IMG_SIZE, IMG_SIZE)
 BATCH_SIZE: int = 32
 
+
 def run():
     # unzip_data("10_food_classes_10_percent.zip")
-    walk_through_dir(LOCAL_DATA_PATH)
+    # walk_through_dir(LOCAL_DATA_PATH)
 
+    # returns BatchDataset, with batch size 32
+    train_data_10_percent = tf.keras.preprocessing.image_dataset_from_directory(directory=TRAIN_DATA_PATH,
+                                                                                label_mode="categorical",
+                                                                                image_size=IMG_SHAPE,
+                                                                                batch_size=BATCH_SIZE)
+    test_data = tf.keras.preprocessing.image_dataset_from_directory(directory=TEST_DATA_PATH,
+                                                                    label_mode="categorical",
+                                                                    image_size=IMG_SHAPE,
+                                                                    batch_size=BATCH_SIZE)
+
+    for images, labels in train_data_10_percent.take(1):
+        print(images, labels)
