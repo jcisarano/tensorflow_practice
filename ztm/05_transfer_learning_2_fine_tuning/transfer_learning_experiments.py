1d1cbcec215db6e90142449d4a77b02f63746026
jcisarano <cisarano@gmail.com>
2021-11-18 17:53:30 -0500

    now really load datasets for experiments

--- ztm/05_transfer_learning_2_fine_tuning/transfer_learning_experiments.py
+++ ztm/05_transfer_learning_2_fine_tuning/transfer_learning_experiments.py
@@ -1,6 +1,20 @@
 
+import  tensorflow as tf
+
 import data_utils as du
 from helper_functions import create_tensorboard_callback, plot_loss_curves, unzip_data, walk_through_dir
 
+
 def run():
-    walk_through_dir(du.LOCAL_DATA_PATH_1_PERCENT)
+    # walk_through_dir(du.LOCAL_DATA_PATH_1_PERCENT)
+
+    train_data_1_percent = tf.keras.preprocessing.image_dataset_from_directory(du.TRAIN_DATA_PATH_1_PERCENT,
+                                                                               label_mode="categorical",
+                                                                               image_size=du.IMG_SHAPE,
+                                                                               batch_size=du.BATCH_SIZE)
+
+    test_data = tf.keras.preprocessing.image_dataset_from_directory(du.TEST_DATA_PATH_1_PERCENT,
+                                                                    label_mode="categorical",
+                                                                    image_size=du.IMG_SHAPE,
+                                                                    batch_size=du.BATCH_SIZE)
+
