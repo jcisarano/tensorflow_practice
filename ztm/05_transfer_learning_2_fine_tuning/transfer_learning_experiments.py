157e264bc2fbf77f6aa03a351ee38c7469c7ffc5
jcisarano <cisarano@gmail.com>
2021-11-18 21:06:50 -0500

    select and show a random image

--- ztm/05_transfer_learning_2_fine_tuning/transfer_learning_experiments.py
+++ ztm/05_transfer_learning_2_fine_tuning/transfer_learning_experiments.py
@@ -4,6 +4,11 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import random
+
 import data_utils as du
 from helper_functions import create_tensorboard_callback, plot_loss_curves, unzip_data, walk_through_dir
 
@@ -36,4 +41,16 @@ def run():
         preprocessing.RandomWidth(0.2),
         # preprocessing.Rescaling(1./255)  # use for models like ResNet50V2, but not EfficientNet
     ], name="data_augmentation")
+
+    # visualize data augmentation
+    # view a random image and compare to augmented version
+    target_class = random.choice(train_data_1_percent.class_names)
+    target_dir = os.path.join(du.TRAIN_DATA_PATH_1_PERCENT, target_class)
+
+    random_image = random.choice(os.listdir(target_dir))
+    random_image_path = os.path.join(target_dir, random_image)
+    img = mpimg.imread(random_image_path)
+    plt.imshow(img)
+    plt.show()
+
 
