e34cfa5cd3805a0d2628420f53f82081ba90fd97
jcisarano <cisarano@gmail.com>
2021-11-24 18:53:49 -0500

    set up model checkpoint

--- ztm/06_transfer_learning_2_scaling_up/food_classifier.py
+++ ztm/06_transfer_learning_2_scaling_up/food_classifier.py
@@ -1,3 +1,15 @@
+"""
+Large model that will classify all 101 food types in Food101 with 10% of training data
+
+Steps to create the model:
+1) Create a ModelCheckpoint callback
+2) Create a data augmentation layer to build data augmentation directly into the model
+3) Build a headless (no top layers) functional EfficientNetB0 backbone model (we will create our oun output model)
+4) Compile
+5) Feature extract for 5 full passes (5 epochs on train set and validate on 15% of test data to save time)
+
+"""
+
 import tensorflow as tf
 
 import data_utils
@@ -13,3 +25,9 @@ def run():
                                                                     image_size=data_utils.IMG_SHAPE,
                                                                     shuffle=False)
 
+    # Create checkpoint callback
+    checkpoint_path = "101_classes_10_percent_data_model_checkpoint"
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                             save_weights_only=True,
+                                                             monitor="val_accuracy",
+                                                             save_best_only=True)
