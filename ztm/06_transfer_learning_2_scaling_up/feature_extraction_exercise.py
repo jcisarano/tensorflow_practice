01551fb17d10b02ca7cdc3f5d1fe12d616505c98
jcisarano <cisarano@gmail.com>
2021-11-27 18:45:48 -0500

    fit & evaluate model, add mixed_precision option

--- ztm/06_transfer_learning_2_scaling_up/feature_extraction_exercise.py
+++ ztm/06_transfer_learning_2_scaling_up/feature_extraction_exercise.py
@@ -4,6 +4,7 @@ import keras
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras import mixed_precision
 
 import data_utils
 
@@ -19,7 +20,11 @@ def create_data_augmentation():
     return data_augmentation
 
 
-def create_model(train_data, test_data):
+def create_model(train_data, test_data, use_mixed_precision=False):
+    if use_mixed_precision:
+        policy = mixed_precision.Policy('mixed_float16')
+        mixed_precision.set_global_policy(policy)
+
     data_aug = create_data_augmentation()
     backbone = tf.keras.applications.EfficientNetB0(include_top=False)
     backbone.trainable = False
@@ -28,12 +33,22 @@ def create_model(train_data, test_data):
     x = data_aug(inputs)
     x = backbone(x, training=False)
     x = tf.keras.layers.GlobalAveragePooling2D(name="global_pooling_layer_2d")(x)
-    outputs = tf.keras.layers.Dense(len(train_data.class_names), activation="softmax", name="output_layer")(x)
+    if use_mixed_precision:
+        outputs = tf.keras.layers.Dense(len(train_data.class_names), activation="softmax", dtype="float32", name="output_layer")(x)
+    else:
+        outputs = tf.keras.layers.Dense(len(train_data.class_names), activation="softmax", name="output_layer")(x)
 
     model = tf.keras.models.Model(inputs, outputs)
     print(model.summary())
 
+    model.compile(loss="categorical_crossentropy", optimizer=tf.keras.optimizers.Adam(), metrics=["accuracy"])
+    history = model.fit(train_data, epochs=10, validation_data=test_data, validation_steps=int(0.25 * len(test_data)),
+                        workers=-1)
+    result = model.evaluate(test_data)
+    print(result)
+
     return model
+
 
 def run():
     train_data_all_10_percent \
@@ -45,6 +60,7 @@ def run():
                                                                     image_size=data_utils.IMG_SHAPE,
                                                                     shuffle=False)
 
-    model = create_model(train_data_all_10_percent, test_data)
+    # create_model(train_data_all_10_percent, test_data)
+    create_model(train_data_all_10_percent, test_data, use_mixed_precision=True)
 
 
