b6a345fd290b7bdad438c6db6a8a9169e10156fc
jcisarano <cisarano@gmail.com>
2021-11-28 17:39:22 -0500

    now load tf food101 dataset

--- ztm/07_milestone_one_food_vision/food_vision_big.py
+++ ztm/07_milestone_one_food_vision/food_vision_big.py
@@ -1,6 +1,28 @@
+"""
+TensorFlow Datasets:
+    ready-to-use datasets
+    Already in tensor format
+    Well-established datasets for many problem types
+    Consistent
+However, TF Datasets are static, they do not change like real-world datasets would
+"""
+
+import tensorflow_datasets as tfds
 
 from helper_functions import compare_histories
 
 
 def run():
-    print("food vision big")
+    # a new way to load food101 dataset, from tensorflow_datasets
+    datasets_list = tfds.list_builders()
+    print("food101" in datasets_list)
+
+    # load the full dataset, but it will take some time because the dataset is 5-6 gb
+    # other datasets can be much larger, so check the dataset size first
+    (train_data, test_data), ds_info = tfds.load(name="food101",
+                                                 split=["train", "validation"],
+                                                 shuffle_files=True,
+                                                 as_supervised=True,  # includes labels in tuple (data,labels)
+                                                 with_info=True  # includes meta data
+                                                 )
+    # print(train_data.shape)
