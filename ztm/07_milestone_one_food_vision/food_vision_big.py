75545f451bb8c5b4c16b2216913400e166826dbb
jcisarano <cisarano@gmail.com>
2021-11-28 10:59:14 -0500

    started milestone one project

---
+++ ztm/07_milestone_one_food_vision/food_vision_big.py
@@ -0,0 +1,6 @@
+
+from helper_functions import compare_histories
+
+
+def run():
+    print("food vision big")
---
+++ ztm/07_milestone_one_food_vision/main.py
@@ -0,0 +1,9 @@
+# Food Vision Milestone Project
+
+import food_vision_big as fvb
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    fvb.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
