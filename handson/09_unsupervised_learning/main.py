f5f3e076d6adcaf1912ed744e9a554562f721bf8
Jason Cisarano <cisarano@gmail.com>
2021-11-30 20:58:13 -0500

    do kmeans prediction

--- handson/09_unsupervised_learning/main.py
+++ handson/09_unsupervised_learning/main.py
@@ -4,6 +4,9 @@ from sklearn.mixture import GaussianMixture
 from scipy import stats
 import numpy as np
 
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
 
 def show_iris_clusters(data):
     x = data.data
@@ -50,10 +53,45 @@ def predict_iris(iris_data):
     print("Percent correct predictions", sum(y_pred == y) / len(y))
 
 
+def plot_clusters(X, y=None):
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=1)
+    plt.xlabel("$x_1$", fontsize=14)
+    plt.ylabel("$x_2$", fontsize=14, rotation=0)
+
+
+def create_blobs():
+    blob_centers = np.array(
+        [[0.2, 2.3],
+         [-1.5, 2.3],
+         [-2.8, 1.8],
+         [-2.8, 2.8],
+         [-2.8, 1.3]])
+    blob_std = np.array([0.4, 0.3, 0.1, 0.1, 0.1])
+    return make_blobs(n_samples=2000, centers=blob_centers, cluster_std=blob_std, random_state=7)
+
+
+def plot_blobs():
+    X, _ = create_blobs()
+    plt.figure(figsize=(8, 4))
+    plot_clusters(X)
+    plt.show()
+
+
+def predict_blobs():
+    X, _ = create_blobs()
+    k = 5
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    y_pred = kmeans.fit_predict(X)
+
+    # view centroids:
+    print(kmeans.cluster_centers_)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data = load_iris()
     # show_iris_clusters(data)
-    predict_iris(data)
+    # predict_iris(data)
+    # plot_blobs()
+    predict_blobs()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
