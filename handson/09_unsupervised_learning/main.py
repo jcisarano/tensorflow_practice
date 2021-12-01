7a0c55d0d48dcdfbbec9938b002cfd1729c95eb8
Jason Cisarano <cisarano@gmail.com>
2021-11-30 19:51:38 -0500

    show iris clusters

--- handson/09_unsupervised_learning/main.py
+++ handson/09_unsupervised_learning/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def show_iris_clusters():
+    data = load_iris()
+    x = data.data
+    y = data.target
+    # print(data.target_names)
 
+    plt.figure(figsize=(9, 3.5))
+    plt.subplot(121)
+    plt.plot(x[y == 0, 2], x[y == 0, 3], "yo", label="Iris setosa")
+    plt.plot(x[y == 1, 2], x[y == 1, 3], "bs", label="Iris veriscolor")
+    plt.plot(x[y == 2, 2], x[y == 2, 3], "g^", label="Iris virginica")
+    plt.xlabel("Petal length", fontsize=14)
+    plt.ylabel("Petal width", fontsize=14)
+    plt.legend(fontsize=12)
+
+    plt.subplot(122)
+    plt.scatter(x[:, 2], x[:, 3], c="k", marker=".")
+    plt.xlabel("Petal length", fontsize=14)
+    plt.tick_params(labelleft=False)
+    plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    show_iris_clusters()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
