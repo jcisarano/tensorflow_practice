f9b594c7b5bf88fd916be1f24679a01b263d90f8
Jason Cisarano <cisarano@gmail.com>
2021-11-30 20:37:27 -0500

    predict and plot iris clusters

--- handson/09_unsupervised_learning/main.py
+++ handson/09_unsupervised_learning/main.py
@@ -1,9 +1,11 @@
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
+from scipy import stats
+import numpy as np
 
 
-def show_iris_clusters():
-    data = load_iris()
+def show_iris_clusters(data):
     x = data.data
     y = data.target
     # print(data.target_names)
@@ -23,8 +25,33 @@ def show_iris_clusters():
     plt.tick_params(labelleft=False)
     plt.show()
 
+
+def predict_iris(iris_data):
+    X = iris_data.data
+    y = iris_data.target
+
+    y_pred = GaussianMixture(n_components=3, random_state=42).fit(X).predict(X)
+    mapping = {}
+    for class_id in np.unique(y):
+        mode, _ = stats.mode(y_pred[y == class_id])
+        mapping[mode[0]] = class_id
+    print(mapping)
+    y_pred = np.array([mapping[cluster_id] for cluster_id in y_pred])
+
+    plt.figure(figsize=(7, 5))
+    plt.plot(X[y_pred == 0, 2], X[y_pred == 0, 3], "yo", label="Cluster 1")
+    plt.plot(X[y_pred == 1, 2], X[y_pred == 1, 3], "bs", label="Cluster 2")
+    plt.plot(X[y_pred == 2, 2], X[y_pred == 2, 3], "g^", label="Cluster 3")
+    plt.xlabel("Petal length", fontsize=14)
+    plt.ylabel("Petal width", fontsize=14)
+    plt.legend(loc="upper left", fontsize=12)
+    plt.show()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    show_iris_clusters()
+    data = load_iris()
+    # show_iris_clusters(data)
+    predict_iris(data)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
