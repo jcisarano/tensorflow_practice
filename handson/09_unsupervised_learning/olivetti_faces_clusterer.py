27c9d887cf0a56e0ec7f35054b008823b1c41bec
jcisarano <cisarano@gmail.com>
2021-12-11 12:51:59 -0500

    different means of setting up train/valid/test sets

--- handson/09_unsupervised_learning/olivetti_faces_clusterer.py
+++ handson/09_unsupervised_learning/olivetti_faces_clusterer.py
@@ -14,7 +14,7 @@ train a model that can predict which person is represented in each picture.
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.datasets import fetch_olivetti_faces
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 
 
 def load_faces():
@@ -25,6 +25,25 @@ def load_faces():
     print(y_test.shape)
 
     return X_train, X_test, y_train, y_test
+
+
+def load_faces_stratified_shuffle():
+    olivetti = fetch_olivetti_faces()
+    strat_split = StratifiedShuffleSplit(n_splits=1, test_size=40, random_state=42)
+    train_valid_idx, test_idx = next(strat_split.split(olivetti.data, olivetti.target))
+    x_train_valid = olivetti.data[train_valid_idx]
+    y_train_valid = olivetti.target[train_valid_idx]
+    X_test = olivetti.data[test_idx]
+    y_test = olivetti.data[test_idx]
+
+    strat_split = StratifiedShuffleSplit(n_splits=1, test_size=80, random_state=43)
+    train_idx, valid_idx = next(strat_split(x_train_valid, y_train_valid))
+    X_train = x_train_valid[train_idx]
+    y_train = y_train_valid[train_idx]
+    X_valid = x_train_valid[valid_idx]
+    y_valid = y_train_valid[valid_idx]
+
+    return X_train, X_valid, X_test, y_train, y_valid, y_test
 
 
 def train_kmeans(X, y, n_clusters=10, random_state=42):
