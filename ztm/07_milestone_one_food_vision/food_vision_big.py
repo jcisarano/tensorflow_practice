306405ade01aedde2bf9e5a202ce39561b47f4e7
jcisarano <cisarano@gmail.com>
2021-12-14 18:15:11 -0500

    image preprocessing

--- ztm/07_milestone_one_food_vision/food_vision_big.py
+++ ztm/07_milestone_one_food_vision/food_vision_big.py
@@ -7,6 +7,26 @@ TensorFlow Datasets:
 However, TF Datasets are static, they do not change like real-world datasets would
 """
 
+"""
+Neural networks perform best when data is formatted properly, e.g. batched, normalized, etc
+However, raw data is rarely formatted as you will need, so you will need to write preprocessing functions.
+
+What we know about our food data:
+    unit8 datatype
+    images are different sizes
+    not normalized-- the color values range from 0 to 255
+    
+However, the models like data in the format:
+    data in float32 type
+    all tensors should be the same size
+    tensors with normalized color values (0-1) usually work better
+    
+We'll use EfficientNetBX pretrained model, so we won't need to rescale data--it has scaling layer built in.
+Our function will need to:
+    1) reshape images
+    2) change datatype from unit8 to float32
+"""
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow_datasets as tfds
@@ -40,6 +60,18 @@ def visualize_data(train_data, test_data, ds_info):
     plt.show()
 
 
+def preprocess_img(image, label, target_img_shape=224):
+    """
+    Converts img datatype to float32 and reshapes image to target_img_shape x target_img_shape X num_color_channels
+    :param image:
+    :param label:
+    :param target_img_shape:
+    :return:
+    """
+    image = tf.image.resize(image, [target_img_shape, target_img_shape])
+    return tf.cast(image, tf.float32)
+
+
 def run():
     # a new way to load food101 dataset, from tensorflow_datasets
     datasets_list = tfds.list_builders()
@@ -55,4 +87,11 @@ def run():
                                                  )
 
     visualize_data(train_data, test_data, ds_info)
+
+    sample = train_data.take(1)
+    for image, label in sample:
+        print(f"Image before preprocessing:\n{image[:2]}...,\nShape: {image.shape},\nDataType: {image.dtype}\n")
+        preprocessed_img = preprocess_img(image, label)
+        print(f"Image after preprocessing:\n{preprocessed_img[:2]}...,\nShape: {preprocessed_img.shape},\nDataType: {preprocessed_img.dtype}\n")
+
 
