1a5fb76b345e3903c311757de5b1b26b5abf95d7
jcisarano <cisarano@gmail.com>
2021-12-20 20:59:33 -0500

    added needed packages to project

---
+++ ztm/08_nlp_intro_in_tf/main.py
@@ -0,0 +1,15 @@
+"""
+NLP has the goal of deriving information out of natural language in the form of sequences of text or speech.
+
+Another common term for NLP problems is sequence to sequence problems (seq2seq) - going from one sequence of data
+to another sequence of data (one to many, many to one, many to many, etc)
+
+"""
+
+import nlp_fundamentals as nlpf
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    nlpf.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ ztm/08_nlp_intro_in_tf/nlp_fundamentals.py
@@ -0,0 +1,12 @@
+"""
+Dataset is Kaggle intro to NLP dataset: tweets labeled as disaster/not disaster. Original source available
+at Kaggle: https://www.kaggle.com/c/nlp-getting-started
+"""
+
+from helper_functions import create_tensorboard_callback, plot_loss_curves, compare_histories
+
+
+def run():
+    print("nlp fundies")
+
+
