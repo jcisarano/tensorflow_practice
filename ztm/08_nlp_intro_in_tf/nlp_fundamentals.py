f5ff931a3ac44bf78188cbbfd2895a8f375a98dc
jcisarano <cisarano@gmail.com>
2021-12-21 08:36:50 -0500

    did train/test split

--- ztm/08_nlp_intro_in_tf/nlp_fundamentals.py
+++ ztm/08_nlp_intro_in_tf/nlp_fundamentals.py
@@ -7,6 +7,7 @@ from helper_functions import create_tensorboard_callback, plot_loss_curves, comp
 
 import pandas as pd
 import random
+from sklearn.model_selection import train_test_split
 
 TRAIN_PATH: str = "datasets/train.csv"
 TEST_PATH: str = "datasets/test.csv"
@@ -15,6 +16,18 @@ TEST_PATH: str = "datasets/test.csv"
 def load_data(train_path=TRAIN_PATH, test_path=TEST_PATH):
     train_df = pd.read_csv(train_path)
     test_df = pd.read_csv(test_path)
+
+    train_df_shuffled = train_df.sample(frac=1, random_state=42)
+    # visualize_train_data(train_df_shuffled)
+
+    train_sentences, val_sentences, train_labels, val_labels = train_test_split(train_df_shuffled["text"].to_numpy(),
+                                                                                train_df_shuffled["target"].to_numpy(),
+                                                                                test_size=0.1,
+                                                                                random_state=42)
+    # print(len(train_sentences), len(train_labels), len(val_sentences), len(val_labels))
+    print(train_sentences[:10], train_labels[:10])
+
+def visualize_train_data(train_df):
     # print(train_df.head)
     # print(train_df["text"][1])
 
@@ -22,12 +35,6 @@ def load_data(train_path=TRAIN_PATH, test_path=TEST_PATH):
     # if it were not, see https://www.tensorflow.org/tutorials/structured_data/imbalanced_data
     # print(train_df.target.value_counts())
 
-    train_df_shuffled = train_df.sample(frac=1, random_state=42)
-
-    # visualize_train_data(train_df_shuffled)
-
-
-def visualize_train_data(train_df):
     # visualize random training examples
     rand_index = random.randint(0, len(train_df)-5)
     for row in train_df[["text", "target"]][rand_index:rand_index+5].itertuples():
