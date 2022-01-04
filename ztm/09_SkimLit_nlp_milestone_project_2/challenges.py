8f8a32d172be88e1191745db5dc731e3a69be55f
jcisarano <cisarano@gmail.com>
2022-01-04 08:57:51 -0500

    added text processors for challenge

--- ztm/09_SkimLit_nlp_milestone_project_2/challenges.py
+++ ztm/09_SkimLit_nlp_milestone_project_2/challenges.py
@@ -9,8 +9,13 @@ Note: Pretrained model stored in Google drive for course if desired:
 """
 
 import tensorflow as tf
+import pandas as pd
+
+from skimlit_text_processors import preprocess_text_with_line_numbers
 
 MODEL_PATH: str = "saved_models/model_5_pos_token_char"
+DATA_DIR_20K_NUM_REPL: str = "dataset/pubmed-rct-master/PubMed_20k_RCT_numbers_replaced_with_at_sign/"
+DATA_DIR_200K_NUM_REPL: str = "dataset/pubmed-rct-master/PubMed_200k_RCT_numbers_replaced_with_at_sign/"
 
 
 def load_model(filepath):
@@ -20,4 +25,8 @@ def load_model(filepath):
 def run():
     model = load_model(MODEL_PATH)
     print(model.summary())
+
+    test_samples = preprocess_text_with_line_numbers(filepath=DATA_DIR_20K_NUM_REPL + "test.txt")
+    test_df = pd.DataFrame(test_samples)
+
     print("challenges")
---
+++ ztm/09_SkimLit_nlp_milestone_project_2/skimlit_text_processors.py
@@ -0,0 +1,59 @@
+
+
+def get_lines(filepath):
+    """
+    Load text file and returns lines in that file as list
+
+    :return:
+        List of strings with one string per line from target file
+    """
+    with open(filepath, "r") as f:
+        return f.readlines()
+
+
+def preprocess_text_with_line_numbers(filepath):
+    """
+    Returns list of dictionaries of abstract line data. Sample data format:
+        [{
+            'line_number' : 0,
+            'target': 'BACKGROUND',
+            'text': 'Emotional eating is associated with overeating and the development of obesity .',
+            'total_lines': 11
+        },
+        ...
+        ]
+    :param filepath:
+    :return:
+    """
+    input_lines = get_lines(filepath)
+    abstract_lines = ""
+    abstract_samples = []
+
+    for line in input_lines:
+        if line.startswith("###"):  # this is an id line, the start of an abstract
+            abstract_id = line
+            abstract_lines = ""
+        elif line.isspace():  # this is an empty line, the end of an abstract
+            abstract_line_split = abstract_lines.splitlines()
+
+            # now iterate through line in a single abstract to handle and count them:
+            for abstract_line_number, abstract_line in enumerate(abstract_line_split):
+                line_data = {}
+                target_text_split = abstract_line.split("\t")  # splits id from text
+                line_data["target"] = target_text_split[0]
+                line_data["text"] = target_text_split[1].lower()
+                line_data["line_number"] = abstract_line_number  # what number is this line in the abstract?
+                line_data["total_lines"] = len(abstract_line_split) - 1
+                abstract_samples.append(line_data)
+        else:
+            abstract_lines += line
+
+    return abstract_samples
+
+
+def get_labels_one_hot(sentences):
+    from sklearn.preprocessing import OneHotEncoder
+    one_hot_encoder = OneHotEncoder(sparse=False)
+    one_hot = one_hot_encoder.fit_transform(sentences)
+
+    return sentences
