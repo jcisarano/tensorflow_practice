d9fb39d2dbac493a853938cd424a203fbb5dd7bd
jcisarano <cisarano@gmail.com>
2022-01-07 18:14:13 -0500

    plot activation functions and their derivatives

---
+++ handson/10_intro_to_ann/activation_functions.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def sigmoid(z):
+    return 1/ (1+np.exp(-z))
+
+
+def relu(z):
+    return np.maximum(0, z)
+
+
+def derivative(f, z, eps=0.000001):
+    return (f(z + eps) - f(z-eps)) / (2*eps)
+
+
+def run():
+    z = np.linspace(-5, 5, 200)
+    plt.figure(figsize=(11, 4))
+
+    plt.subplot(121)
+    plt.plot(z, np.sign(z), "r-", linewidth=1, label="Step")
+    plt.plot(z, sigmoid(z), "g--", linewidth=2, label="Sigmoid")
+    plt.plot(z, np.tanh(z), "b-", linewidth=2, label="Tanh")
+    plt.plot(z, relu(z), "m-.", linewidth=2, label="ReLU")
+    plt.grid(True)
+    plt.legend(loc="center right", fontsize=14)
+    plt.title("Activation functions", fontsize=14)
+    plt.axis([-5, 5, -1.2, 1.2])
+
+    plt.subplot(122)
+    plt.plot(z, derivative(np.sign, z), "r-", linewidth=1, label="Step")
+    plt.plot(0, 0, "ro", markersize=5)
+    plt.plot(0, 0, "rx", markersize=10)
+    plt.plot(z, derivative(sigmoid, z), "g--", linewidth=2, label="Sigmoid")
+    plt.plot(z, derivative(np.tanh, z), "b-", linewidth=2, label="Tanh")
+    plt.plot(z, derivative(relu, z), "m-.", linewidth=2, label="ReLU")
+    plt.grid(True)
+
+    plt.title("Derivatives", fontsize=14)
+    plt.axis([-5, 5, -0.2, 1.2])
+
+    plt.show()
+
--- handson/10_intro_to_ann/main.py
+++ handson/10_intro_to_ann/main.py
@@ -1,8 +1,10 @@
 
 import perceptrons as pcpt
+import activation_functions as af
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pcpt.run()
+    # pcpt.run()
+    af.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
