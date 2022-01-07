c7bad316f95a5d048d28c55f773a6341ae6c849d
jcisarano <cisarano@gmail.com>
2022-01-07 18:01:58 -0500

    started ann project with single perceptron example

---
+++ handson/10_intro_to_ann/main.py
@@ -0,0 +1,8 @@
+
+import perceptrons as pcpt
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pcpt.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
