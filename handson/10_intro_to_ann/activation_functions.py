bb36d4ed68d646756bad1fb5d59dfba2e8840484
jcisarano <cisarano@gmail.com>
2022-01-07 18:21:43 -0500

    plot more activation functions

--- handson/10_intro_to_ann/activation_functions.py
+++ handson/10_intro_to_ann/activation_functions.py
@@ -14,6 +14,14 @@ def derivative(f, z, eps=0.000001):
     return (f(z + eps) - f(z-eps)) / (2*eps)
 
 
+def heaviside(z):
+    return (z >= 0).astype(z.dtype)
+
+
+def mlp_xor(x1, x2, activation=heaviside):
+    return activation(-activation(x1+x2-1.5) + activation(x1+x2-0.5)-0.5)
+
+
 def run():
     z = np.linspace(-5, 5, 200)
     plt.figure(figsize=(11, 4))
@@ -41,4 +49,28 @@ def run():
     plt.axis([-5, 5, -0.2, 1.2])
 
     plt.show()
+
+    x1s = np.linspace(-0.2, 1.2, 100)
+    x2s = np.linspace(-0.2, 1.2, 100)
+    x1, x2 = np.meshgrid(x1s, x2s)
+
+    z1 = mlp_xor(x1, x2, activation=heaviside)
+    z2 = mlp_xor(x1, x2, activation=sigmoid)
+
+    plt.figure(figsize=(10, 4))
+
+    plt.subplot(121)
+    plt.contourf(x1, x2, z1)
+    plt.plot([0, 1], [0, 1], "gs", markersize=20)
+    plt.plot([0, 1], [1, 0], "y^", markersize=20)
+    plt.title("Activation function: heaviside", fontsize=14)
+    plt.grid(True)
+
+    plt.subplot(122)
+    plt.contourf(x1, x2, z2)
+    plt.plot([0, 1], [0, 1], "gs", markersize=20)
+    plt.plot([0, 1], [1, 0], "y^", markersize=20)
+    plt.title("Activation function: sigmoid", fontsize=14)
+    plt.show()
+
 
