ecfd3a75ab1df53de482aa9a7344e2f2ef24a301
jcisarano <cisarano@gmail.com>
2022-01-10 08:17:30 -0500

    model with separate outputs for regularization

--- handson/10_intro_to_ann/functional_api.py
+++ handson/10_intro_to_ann/functional_api.py
@@ -71,3 +71,22 @@ def run():
     print("MSE TEST subset model:", mse_test)
     print("y_pred subset model:", y_pred)
 
+
+    # ###############################
+    # adds output for regularization:
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    input_A = keras.layers.Input(shape=[5], name="wide_input")
+    input_B = keras.layers.Input(shape=[6], name="deep_input")
+    hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+    hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+    concat = keras.layers.concatenate([input_A, hidden2])
+    output = keras.layers.Dense(1, name="main_output")(concat)
+    aux_output = keras.layers.Dense(1, name="aux_output")(hidden2)
+    model = keras.models.Model(inputs=[input_A, input_B], outputs=[output, aux_output])
+    model.compile(loss=["mse", "mse"], loss_weights=[0.9, 0.1], optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+    print(model.summary())
+
+    history = model.fit([X_train_A, X_train_B], [y_train, y_train], epochs=20,
+                        validation_data=([X_valid_A, X_valid_B], [y_valid, y_valid]))
