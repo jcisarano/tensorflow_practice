a52068552fccf64d4abb096e75276e4c8793b4fe
jcisarano <cisarano@gmail.com>
2022-01-10 08:13:34 -0500

    model using subsed of input features

--- handson/10_intro_to_ann/functional_api.py
+++ handson/10_intro_to_ann/functional_api.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def run():
     np.random.seed(42)
     tf.random.set_seed(42)
@@ -43,3 +44,30 @@ def run():
     plt.grid(True)
     plt.gca().set_ylim(0, 1)
     plt.show()
+
+    # #####
+    # different subsets of input features
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    input_A = keras.layers.Input(shape=[5], name="wide_output")
+    input_B = keras.layers.Input(shape=[6], name="deep_input")
+    hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+    hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+    concat = keras.layers.concatenate([input_A, hidden2])
+    output = keras.layers.Dense(1, name="output")(concat)
+    model = keras.models.Model(inputs=[input_A, input_B], outputs=[output])
+
+    model.compile(loss="mse", optimizer=keras.optimizers.SGD(learning_rate=1e-3))
+    X_train_A, X_train_B = X_train[:, :5], X_train[:, 2:]
+    X_valid_A, X_valid_B = X_valid[:, :5], X_valid[:, 2:]
+    X_test_A, X_test_B = X_test[:, :5], X_test[:, 2:]
+    X_new_A, X_new_B = X_test_A[:3], X_test_B[:3]
+
+    history = model.fit((X_train_A, X_train_B), y_train, epochs=20,
+                        validation_data=((X_valid_A, X_valid_B), y_valid))
+    mse_test = model.evaluate((X_test_A, X_test_B), y_test)
+    y_pred = model.predict((X_new_A, X_new_B))
+    print("MSE TEST subset model:", mse_test)
+    print("y_pred subset model:", y_pred)
+
