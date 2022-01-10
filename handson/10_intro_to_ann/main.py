3a393758c971fd6aae8a637de5369aa914ae6a1c
jcisarano <cisarano@gmail.com>
2022-01-10 18:13:35 -0500

    subclassing api example

--- handson/10_intro_to_ann/main.py
+++ handson/10_intro_to_ann/main.py
@@ -4,6 +4,7 @@ import activation_functions as af
 import image_classifier as ic
 import regression_mlp as rmlp
 import functional_api as fa
+import subclassing_api as sub
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -11,6 +12,7 @@ if __name__ == '__main__':
     # af.run()
     # ic.run()
     # rmlp.run()
-    fa.run()
+    # fa.run()
+    sub.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
