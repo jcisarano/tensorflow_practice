ea4d603e1d68443e467dea57a0e7a9cfd0016891
jcisarano <cisarano@gmail.com>
2022-01-13 17:26:56 -0500

    set up data

--- handson/10_intro_to_ann/hyperparameter_tuning.py
+++ handson/10_intro_to_ann/hyperparameter_tuning.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 import keras.models
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from tensorflow.keras import  layers
 
 
@@ -21,5 +24,14 @@ def run():
     np.random.seed(42)
     tf.random.set_seed(42)
 
+    housing = fetch_california_housing()
+    X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target, random_state=42)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_valid = scaler.transform(X_valid)
+    X_test = scaler.transform(X_test)
+
+    keras_reg = tf.keras.wrappers.scikit_learn.KerasRegressor(build_model)
     print("yperparameter tuning")
 
