e5640f51290481255a35615bd4c43b55ed18eb2c
jcisarano <cisarano@gmail.com>
2022-01-15 17:34:40 -0500

    plot leaky relu

--- handson/11_training_deep_nn/vanishing_gradients.py
+++ handson/11_training_deep_nn/vanishing_gradients.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
+from tensorflow import keras
 
 def logit(z):
     return 1 / (1+np.exp(-z))
@@ -27,5 +28,38 @@ def plot_sigmoid_function():
 
     plt.show()
 
+
+def explore_keras_initializers():
+    # list built-in kernel initializers:
+    print([name for name in dir(keras.initializers) if not name.startswith("_")])
+    # use one via string name:
+    print(keras.layers.Dense(10, activation="relu", kernel_initializer="he_normal"))
+    # use via api
+    init = keras.initializers.VarianceScaling(scale=2., mode="fan_avg", distribution="uniform")
+    print(keras.layers.Dense(10, activation="relu", kernel_initializer=init))
+
+
+def leaky_relu(z, alpha=0.01):
+    return np.maximum(alpha*z, z)
+
+
+def plot_leaky_relu():
+    z = np.linspace(-5, 5, 200)
+
+    plt.plot([-5, 5], [0, 0], 'k-')
+    plt.plot([0, 0], [-0.5, 4.2], 'k-')
+    plt.plot(z, leaky_relu(z, 0.05), 'b-', linewidth=2)
+
+    props = dict(facecolor='black', shrink=0.1)
+    plt.annotate("Leak", xytext=[-3.5, 0.5], xy=[-5, -0.2], arrowprops=props, fontsize=14, ha="center")
+    plt.axis([-5, 5, -0.5, 4.2])
+    plt.grid(True)
+    plt.title("Leaky ReLU activation function")
+    plt.show()
+
+
 def run():
-    plot_sigmoid_function()
+    # plot_sigmoid_function()
+    # explore_keras_initializers()
+    plot_leaky_relu()
+
