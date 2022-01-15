b1541aa8ce0a169e728d5675b0d8e2d835229338
jcisarano <cisarano@gmail.com>
2022-01-15 17:19:11 -0500

    plot sigmoid function

---
+++ handson/11_training_deep_nn/main.py
@@ -0,0 +1,7 @@
+
+import vanishing_gradients as vg
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    vg.run()
+
---
+++ handson/11_training_deep_nn/vanishing_gradients.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def logit(z):
+    return 1 / (1+np.exp(-z))
+
+
+def plot_sigmoid_function():
+    z = np.linspace(-5, 5, 200)
+
+    plt.plot([-5, 5], [0, 0], 'k-')
+    plt.plot([-5, 5], [1, 1], 'k--')
+    plt.plot([0, 0], [-0.2, 1.2], 'k-')
+    plt.plot([-5, 5], [-3/4, 7/4], 'g--')
+    plt.plot(z, logit(z), 'b-', linewidth=2)
+
+    props = dict(facecolor='black', shrink=0.1)
+    plt.annotate('Saturating', xytext=(3.5, 0.7), xy=(5, 1), arrowprops=props, fontsize=14, ha="center")
+    plt.annotate('Saturating', xytext=(-3.5, 0.3), xy=(-5, 0), arrowprops=props, fontsize=14, ha="center")
+    plt.annotate('Linear', xytext=(2, 0.2), xy=(0, 0.5), arrowprops=props, fontsize=14, ha="center")
+
+    plt.grid(True)
+    plt.title("Sigmoid activation function")
+    plt.axis([-5, 5, -0.2, 1.2])
+
+    plt.show()
+
+def run():
+    plot_sigmoid_function()
