c3f245b72d6d1096cee2317fee0a60083e453c6e
jcisarano <cisarano@gmail.com>
2022-01-16 10:09:23 -0500

    train prelu model

--- handson/11_training_deep_nn/vanishing_gradients.py
+++ handson/11_training_deep_nn/vanishing_gradients.py
@@ -73,6 +73,28 @@ def load_data():
     return X_train, X_valid, X_test, y_train, y_valid, y_test
 
 
+def train_fashion_mnist_prelu():
+    X_train, X_valid, X_test, y_train, y_valid, y_test = load_data()
+
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.Dense(300, kernel_initializer="he_normal"),
+        keras.layers.PReLU(),
+        keras.layers.Dense(100, kernel_initializer="he_normal"),
+        keras.layers.PReLU(),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    model.compile(loss="sparse_categorical_crossentropy",
+                  optimizer=tf.keras.optimizers.SGD(learning_rate=1e-3),
+                  metrics=["accuracy"])
+    model.fit(X_train, y_train, epochs=10,
+              validation_data=[X_valid, y_valid])
+
+
 def train_fashion_mnist_relu():
     X_train, X_valid, X_test, y_train, y_valid, y_test = load_data()
 
@@ -102,5 +124,6 @@ def run():
     # explore_keras_initializers()
     # plot_leaky_relu()
     # list_keras_activations()
-    train_fashion_mnist_relu()
+    # train_fashion_mnist_relu()
+    train_fashion_mnist_prelu()
 
