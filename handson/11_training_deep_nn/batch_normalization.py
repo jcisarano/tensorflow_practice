3f374329d1503835b88ae49f7494b5205cbc0bd7
jcisarano <cisarano@gmail.com>
2022-01-17 08:21:47 -0500

    move model create to function, add new model type

--- handson/11_training_deep_nn/batch_normalization.py
+++ handson/11_training_deep_nn/batch_normalization.py
@@ -5,9 +5,7 @@ from tensorflow import keras
 from helper_functions import load_data
 
 
-def run():
-    X_train, X_valid, X_test, y_train, y_valid, y_test = load_data()
-
+def batch_norm_model():
     model = keras.models.Sequential([
         keras.layers.Flatten(input_shape=[28, 28]),
         keras.layers.BatchNormalization(),
@@ -17,6 +15,32 @@ def run():
         keras.layers.BatchNormalization(),
         keras.layers.Dense(10, activation="softmax")
     ])
+
+    return model
+
+
+def batch_norm_model_separate_activation():
+    model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=[28, 28]),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(300, use_bias=False),
+        keras.layers.BatchNormalization(),
+        keras.layers.Activation("relu"),
+        keras.layers.Dense(100, use_bias=False),
+        keras.layers.BatchNormalization(),
+        keras.layers.Activation("relu"),
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    return model
+
+
+def run():
+    X_train, X_valid, X_test, y_train, y_valid, y_test = load_data()
+
+    # model = batch_norm_model()
+    model = batch_norm_model_separate_activation()
+
     # print(model.summary())
     # bn1 = model.layers[1]
     # print([(var.name, var.trainable) for var in bn1.variables])
