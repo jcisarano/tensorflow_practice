0ef38e90d8df41e6d8df8970721f37915641cdcf
jcisarano <cisarano@gmail.com>
2022-01-18 08:33:12 -0500

    power scheduling

--- handson/11_training_deep_nn/learning_rate_scheduler.py
+++ handson/11_training_deep_nn/learning_rate_scheduler.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 
@@ -30,6 +31,22 @@ def run():
                         validation_data=(X_valid_scaled, y_valid),
                         workers=-1)
 
+    # graph power scheduling
+    learning_rate = 0.01
+    decay = 1e-4
+    batch_size = 32
+    n_steps_per_epoch = math.ceil(len(X_train) / batch_size)
+    epochs = np.arange(n_epochs)
+    lrs = learning_rate / (1 + decay*epochs*n_steps_per_epoch)
+
+    plt.plot(epochs, lrs, "o-")
+    plt.axis([0, n_epochs-1, 0, 0.01])
+    plt.title("Power Scheduling", fontsize=14)
+    plt.xlabel("Epoch")
+    plt.ylabel("Learning Rate")
+    plt.grid(True)
+
+    plt.show()
 
 
     print("lr scheduling")
