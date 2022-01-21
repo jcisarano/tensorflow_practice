0dc2455c2fe9ede5ef0a6c39d22cde18777d5b81
jcisarano <cisarano@gmail.com>
2022-01-21 08:08:52 -0500

    addes OneCycleScheduler class

--- handson/11_training_deep_nn/onecycle_scheduling.py
+++ handson/11_training_deep_nn/onecycle_scheduling.py
@@ -22,6 +22,31 @@ K = tf.keras.backend
         K.set_value(self.model.optimizer.learning_rate, self.model.optimizer.learning_rate * self.factor)"""
 
 
+class OneCycleScheduler(keras.callbacks.Callback):
+    def __init__(self, iterations, max_rate, start_rate=None,
+                 last_iterations=None, last_rate=None):
+        self.iterations = iterations
+        self.max_rate = max_rate
+        self.start_rate = start_rate or max_rate / 10
+        self.last_iterations = last_iterations or iterations // 10 + 1
+        self.half_iteration = (iterations - self.last_iterations) // 2
+        self.last_rate = last_rate  or self.start_rate / 1000
+        self.iteration = 0
+
+    def _interpolate(self, iter1, iter2, rate1, rate2):
+        return ((rate2 - rate1)*(self.iteration - iter1) / (iter2 - iter1) + rate1)
+
+    def on_batch_begin(self, batch, logs):
+        if self.iteration < self.half_iteration:
+            rate = self._interpolate(0, self.half_iteration, self.start_rate, self.max_rate)
+        elif self.iteration < 2*self.half_iteration:
+            rate = self._interpolate(self.half_iteration, 2*self.half_iteration, self.max_rate, self.start_rate)
+        else:
+            rate = self._interpolate((2*self.half_iteration, self.iterations, self.start_rate, self.last_rate))
+        self.iteration += 1
+        K.set_value(self.model.optimizer.learning_rate, rate)
+
+
 class ExponentialLearningRate(tf.keras.callbacks.Callback):
     def __init__(self, factor):
         self.factor = factor
