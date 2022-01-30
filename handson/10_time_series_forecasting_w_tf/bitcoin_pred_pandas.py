fd4e41ee6dabd95c81878373d1d8e1c3663afffb
jcisarano <cisarano@gmail.com>
2022-01-30 11:56:55 -0500

    load and explore bitcoin data

---
+++ handson/10_time_series_forecasting_w_tf/bitcoin_pred_pandas.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+
+DATA_PATH: str = "data/BTC_USD_2013-10-01_2021-05-18-CoinDesk.csv"
+
+
+def run():
+    df = pd.read_csv(DATA_PATH,
+                     parse_dates=["Date"],  # parses the date column to pandas.datetime
+                     index_col=["Date"])  # makes the date column the index, useful because this is sequential data
+
+    print(df.head())  # first few entries
+    print(df.tail())  # last few entries
+    print(df.info())  # data types
+    print(len(df))  # how many samples
+    # only about 8 years of daily data, so not many entries - small datasets common with time series data
+    # "seasonality" of time series datasets is the number of samples per year. For this Bitcoin data, the seasonality
+    # is daily, so only 365 samples per year
+
+    print("bitcoin predict pandas")
--- handson/10_time_series_forecasting_w_tf/main.py
+++ handson/10_time_series_forecasting_w_tf/main.py
@@ -1,8 +1,10 @@
 # Time series forecasting fundamentals with TensorFlow plus Milestone Project 3: BitPredict
 
 
+import bitcoin_pred_pandas as bpp
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("hello")
+    bpp.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
