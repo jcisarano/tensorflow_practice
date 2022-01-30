ec0e1f4bf4a15e75f44bc16b5fcb2ddfa8665f43
jcisarano <cisarano@gmail.com>
2022-01-30 16:29:33 -0500

    load data using csv lib and plot it

--- handson/10_time_series_forecasting_w_tf/bitcoin_pred_pandas.py
+++ handson/10_time_series_forecasting_w_tf/bitcoin_pred_pandas.py
@@ -1,8 +1,51 @@
+from datetime import datetime
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 DATA_PATH: str = "data/BTC_USD_2013-10-01_2021-05-18-CoinDesk.csv"
+
+
+def load_csv():
+    timesteps = []
+    btc_price = []
+    with open(DATA_PATH, mode="r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        next(csv_reader)
+        data = list(csv_reader)
+    for item in data:
+        timesteps.append(item[1])
+        btc_price.append(item[2])
+    print(btc_price)
+    data_array = np.concatenate([timesteps, btc_price])
+    return data_array
+
+
+def load_csv_1():
+    timesteps = []
+    btc_price = []
+    with open(DATA_PATH, "r") as f:
+        csv_reader = csv.reader(f, delimiter=",")
+        next(csv_reader)
+        for line in csv_reader:
+            timesteps.append(datetime.strptime(line[1], "%Y-%m-%d"))  # date as datetime object
+            btc_price.append(float(line[2]))  # price as float
+    print(timesteps[:10])
+    print(btc_price[:10])
+
+    plt.figure(figsize=[10, 7])
+    plt.plot(timesteps, btc_price)
+    plt.title(label="Price of Bitcoin from 1 Oct 2013 to 18 May 2021", fontsize=16)
+    plt.ylabel("BTC Price", fontsize=12)
+    plt.xlabel("Date", fontsize=12)
+    plt.legend(fontsize=14)
+    axes = plt.gca().axes
+    axes.yaxis.grid()
+    axes.tick_params(axis="x", labelsize=10)
+    axes.tick_params(axis="y", labelsize=10)
+    plt.show()
 
 
 def visualize(df):
@@ -14,24 +57,26 @@ def visualize(df):
     # "seasonality" of time series datasets is the number of samples per year. For this Bitcoin data, the seasonality
     # is daily, so only 365 samples per year
 
-    df.plot(title="Price of Bitcoin from 1 Oct 2013 to 18 May 2021", fontsize=16, figsize=[10,7])
+    df.plot(title="Price of Bitcoin from 1 Oct 2013 to 18 May 2021", fontsize=16, figsize=[10, 7])
     plt.ylabel("BTC Price", fontsize=12)
     plt.xlabel("Date", fontsize=12)
     plt.legend(fontsize=14)
     axes = plt.gca().axes
     axes.yaxis.grid()
-    # plt.setp(axes.get_xticklabels(), fontsize=10)
     axes.tick_params(axis="x", labelsize=10)
     axes.tick_params(axis="y", labelsize=10)
     plt.show()
 
 
 def run():
-    df = pd.read_csv(DATA_PATH,
-                     parse_dates=["Date"],  # parses the date column to pandas.datetime
-                     index_col=["Date"])  # makes the date column the index, useful because this is sequential data
+    # df = pd.read_csv(DATA_PATH,
+    #                  parse_dates=["Date"],  # parses the date column to pandas.datetime
+    #                  index_col=["Date"])  # makes the date column the index, useful because this is sequential data
 
-    bitcoin_prices = pd.DataFrame(df["Closing Price (USD)"]).rename(columns={"Closing Price (USD)": "Price"})
-    visualize(bitcoin_prices)
+    # bitcoin_prices = pd.DataFrame(df["Closing Price (USD)"]).rename(columns={"Closing Price (USD)": "Price"})
+    # visualize(bitcoin_prices)
+    #prices = load_csv()
+    #print(prices)
+    load_csv_1()
 
     print("bitcoin predict pandas")
