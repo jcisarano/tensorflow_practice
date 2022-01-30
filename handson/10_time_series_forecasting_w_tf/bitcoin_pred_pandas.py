ccd2f65ce52b93d65a77b6c7758a97fc22ced601
jcisarano <cisarano@gmail.com>
2022-01-30 15:25:58 -0500

    plot df, move to function

--- handson/10_time_series_forecasting_w_tf/bitcoin_pred_pandas.py
+++ handson/10_time_series_forecasting_w_tf/bitcoin_pred_pandas.py
@@ -1,14 +1,11 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 DATA_PATH: str = "data/BTC_USD_2013-10-01_2021-05-18-CoinDesk.csv"
 
 
-def run():
-    df = pd.read_csv(DATA_PATH,
-                     parse_dates=["Date"],  # parses the date column to pandas.datetime
-                     index_col=["Date"])  # makes the date column the index, useful because this is sequential data
-
+def visualize(df):
     print(df.head())  # first few entries
     print(df.tail())  # last few entries
     print(df.info())  # data types
@@ -16,5 +13,16 @@ def run():
     # only about 8 years of daily data, so not many entries - small datasets common with time series data
     # "seasonality" of time series datasets is the number of samples per year. For this Bitcoin data, the seasonality
     # is daily, so only 365 samples per year
+
+    df.plot(y="Closing Price (USD)")
+
+
+def run():
+    df = pd.read_csv(DATA_PATH,
+                     parse_dates=["Date"],  # parses the date column to pandas.datetime
+                     index_col=["Date"])  # makes the date column the index, useful because this is sequential data
+    visualize(df)
+
+    plt.show()
 
     print("bitcoin predict pandas")
