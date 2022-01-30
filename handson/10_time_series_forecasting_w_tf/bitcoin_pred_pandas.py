1cd8404d2e850bc693f82560585ed56c0a9ad7ad
jcisarano <cisarano@gmail.com>
2022-01-30 15:50:05 -0500

    clean up plot

--- handson/10_time_series_forecasting_w_tf/bitcoin_pred_pandas.py
+++ handson/10_time_series_forecasting_w_tf/bitcoin_pred_pandas.py
@@ -14,15 +14,24 @@ def visualize(df):
     # "seasonality" of time series datasets is the number of samples per year. For this Bitcoin data, the seasonality
     # is daily, so only 365 samples per year
 
-    df.plot(y="Closing Price (USD)")
+    df.plot(title="Price of Bitcoin from 1 Oct 2013 to 18 May 2021", fontsize=16, figsize=[10,7])
+    plt.ylabel("BTC Price", fontsize=12)
+    plt.xlabel("Date", fontsize=12)
+    plt.legend(fontsize=14)
+    axes = plt.gca().axes
+    axes.yaxis.grid()
+    # plt.setp(axes.get_xticklabels(), fontsize=10)
+    axes.tick_params(axis="x", labelsize=10)
+    axes.tick_params(axis="y", labelsize=10)
+    plt.show()
 
 
 def run():
     df = pd.read_csv(DATA_PATH,
                      parse_dates=["Date"],  # parses the date column to pandas.datetime
                      index_col=["Date"])  # makes the date column the index, useful because this is sequential data
-    visualize(df)
 
-    plt.show()
+    bitcoin_prices = pd.DataFrame(df["Closing Price (USD)"]).rename(columns={"Closing Price (USD)": "Price"})
+    visualize(bitcoin_prices)
 
     print("bitcoin predict pandas")
