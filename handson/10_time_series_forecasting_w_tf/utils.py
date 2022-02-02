7647e49bc971eb0e1fe644d02b14a7d1eb880d98
jcisarano <cisarano@gmail.com>
2022-02-02 08:05:47 -0500

    added eval function to utils

--- handson/10_time_series_forecasting_w_tf/utils.py
+++ handson/10_time_series_forecasting_w_tf/utils.py
@@ -50,8 +50,25 @@ def mean_absolute_scaled_error(y_true, y_pred):
     :param y_pred:
     :return:
     """
-    mae = tf.reduce_mean(tf.abs(y_true-y_pred))
+    mae = tf.reduce_mean(tf.abs(y_true - y_pred))
     mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1]))  # shift left 1 for seasonality of one day
 
-    return  mae / mae_naive_no_season
+    return mae / mae_naive_no_season
 
+
+def evaluate_preds(y_true, y_pred):
+    # make sure everything is float32
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+    mae = tf.keras.metrics.mean_absolute_error(y_true, y_pred)
+    mse = tf.keras.metrics.mean_squared_error(y_true, y_pred)
+    rmse = tf.sqrt(mse)
+    mape = tf.keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+    mase = mean_absolute_scaled_error(y_true, y_pred)
+
+    return {"mae": mae.numpy(),
+            "mse": mse.numpy(),
+            "rmse": rmse.numpy(),
+            "mape": mape.numpy(),
+            "mase": mase.numpy()}
