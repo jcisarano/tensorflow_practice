d42c79d0195b5f61c9d97d0e1bb4e95952f07b6b
jcisarano <cisarano@gmail.com>
2022-02-06 18:59:43 -0500

    created expand dims layer

--- handson/10_time_series_forecasting_w_tf/model_4_conv1d.py
+++ handson/10_time_series_forecasting_w_tf/model_4_conv1d.py
@@ -7,11 +7,24 @@ from utils import load_data, my_train_test_split, make_windows, make_train_test_
 HORIZON: int = 1
 WINDOW_SIZE: int = 7
 
+
 def run():
     timesteps, prices = load_data()
     X_train, X_test, y_train, y_test = my_train_test_split(timesteps, prices)
     full_windows, full_labels = make_windows(prices, window_size=WINDOW_SIZE, horizon=HORIZON)
+    print(len(full_windows), len(full_labels))
     train_windows, test_windows, train_labels, test_labels = make_train_test_splits(full_windows, full_labels)
+    print(len(train_windows), len(train_labels), len(test_windows), len(test_labels))
 
     tf.random.set_seed(42)
     model_name = "model_1_dense"
+
+    # input for Conv1D layer must be shape (batch_size, timesteps, input_dim)
+    print(train_windows[0].shape)
+    # so we must reshape it to fit our needs
+    x = tf.constant(train_windows[0])
+    expand_dims_layer = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=1))
+    # lambda layer will turn python lambda function into a layer
+    print(f"Original shape: {x.shape}")
+    print(f"Expanded shape: {expand_dims_layer(x).shape}")  # adds extra dimension
+    print(f"Original values with expanded shape  {expand_dims_layer(x)}")
