a3d1cf1568824010c7e78ae38fdb8b34a494b318
jcisarano <cisarano@gmail.com>
2022-02-17 08:31:30 -0500

    make future preds

--- handson/10_time_series_forecasting_w_tf/model_9_future_prediction.py
+++ handson/10_time_series_forecasting_w_tf/model_9_future_prediction.py
@@ -2,15 +2,21 @@
 Previous models predicted based on test dataset, but that is not truly future predictions. This model
 will make predictions into the future.
 
+For time series forecasts, you have to retrain the model every time you want to make a prediction. See:
+https://towardsdatascience.com/3-facts-about-time-series-forecasting-that-surprise-experienced-machine-learning-practitioners-69c18ee89387
+
 """
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 import utils
 
 BATCH_SIZE: int = 1024
 WINDOW_SIZE = 7
 HORIZON = 1
+
+INTO_FUTURE = 14
 
 
 def make_prices_windowed(window_size=WINDOW_SIZE, horizon=HORIZON):
@@ -19,9 +25,10 @@ def make_prices_windowed(window_size=WINDOW_SIZE, horizon=HORIZON):
         bitcoin_prices_windowed[f"Price+{i+1}"] = bitcoin_prices_windowed["Price"].shift(periods=i+1)
 
     # print(bitcoin_prices_windowed.head)
+    print(bitcoin_prices_windowed.tail())
 
     X_all = bitcoin_prices_windowed.dropna().drop(["Price", "block_reward"], axis=1).astype(np.float32)
-    y_all = bitcoin_prices_windowed.dropna()["Price"].astype(np.float32)
+    y_all = bitcoin_prices_windowed.dropna()["Price"].to_numpy()
 
     # print(X.head)
     # print(y.head)
@@ -36,10 +43,10 @@ def make_prices_windowed(window_size=WINDOW_SIZE, horizon=HORIZON):
 
     # print(dataset_all)
 
-    return dataset_all
+    return dataset_all, X_all, y_all
 
 
-def create_model(train_dataset):
+def create_model(train_dataset, X_all, y_all):
     model = tf.keras.Sequential([
         tf.keras.layers.Dense(128, activation="relu"),
         tf.keras.layers.Dense(128, activation="relu"),
@@ -49,10 +56,31 @@ def create_model(train_dataset):
     model.compile(loss="MAE", optimizer=tf.keras.optimizers.Adam())
     model.fit(train_dataset, epochs=100)
 
+    future_forecast = make_future_forecast(historical_dataset=y_all, model=model)
+
+    return future_forecast
+
+
+def make_future_forecast(historical_dataset, model, into_future=INTO_FUTURE, window_size=WINDOW_SIZE) -> list:
+    """
+    Make future forecasts into_future steps after values end.
+    Returns future forecasts as a list of floats
+    """
+    future_forecasts = []
+    last_window = historical_dataset[-window_size:]
+
+    for _ in range(into_future):
+        future_pred = model.predict(tf.expand_dims(last_window, axis=0))
+        print(f"Predicting on:\n{last_window} -> Prediction: {tf.squeeze(future_pred).numpy()}\n")
+        future_forecasts.append(tf.squeeze(future_pred).numpy())
+        last_window = np.append(last_window, future_pred)[-window_size:]
+
+    return future_forecasts
+
 
 def run():
-    train_dataset = make_prices_windowed()
-    create_model(train_dataset)
+    train_dataset, X_all, y_all = make_prices_windowed()
+    create_model(train_dataset, X_all, y_all)
 
 
     print("fut pred")
