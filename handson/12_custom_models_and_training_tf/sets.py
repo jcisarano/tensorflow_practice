25c27881bb5142880798d6b0415cbf3aa89d6344
jcisarano <cisarano@gmail.com>
2022-02-23 08:04:31 -0500

    added notes

--- handson/12_custom_models_and_training_tf/sets.py
+++ handson/12_custom_models_and_training_tf/sets.py
@@ -7,12 +7,16 @@ import tensorflow as tf
 def run():
     set1 = tf.constant([[2, 3, 5, 7], [7, 9, 0, 0]])
     set2 = tf.constant([[4, 5, 6], [9, 10, 0]])
+
+    # union of sets in last dimension
     print("\ntf.sparse.to_dense(tf.sets.union(set1, set2))\n",
           tf.sparse.to_dense(tf.sets.union(set1, set2)))
 
+    # set difference of elements in last dimension. All but the last dimension of sets must match
     print("\ntf.sparse.to_dense(tf.sets.difference(set1, set2))\n",
           tf.sparse.to_dense(tf.sets.difference(set1, set2)))
 
+    # Computes set intersection of elements in last dimension. All but last dimension of sets must match
     print("\ntf.sparse.to_dense(tf.sets.intersection(set1, set2))\n",
           tf.sparse.to_dense(tf.sets.intersection(set1, set2)))
 
