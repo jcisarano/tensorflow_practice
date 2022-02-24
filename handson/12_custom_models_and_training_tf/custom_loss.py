236601c774e60593b27a4de8d286436040754a45
jcisarano <cisarano@gmail.com>
2022-02-24 08:48:01 -0500

    create model, compile, train

--- handson/12_custom_models_and_training_tf/custom_loss.py
+++ handson/12_custom_models_and_training_tf/custom_loss.py
@@ -21,7 +21,7 @@ def load_and_prep_data():
     X_valid_scaled = scaler.transform(X_valid)
     X_test_scaled = scaler.transform(X_test)
 
-    return X_test_scaled, X_valid_scaled, X_test_scaled, y_train, y_valid, y_test
+    return X_train_scaled, X_valid_scaled, X_test_scaled, y_train, y_valid, y_test
 
 
 def huber_fn(y_true, y_pred):
@@ -51,8 +51,16 @@ def plot_loss_function():
 
 
 def run():
-    X_test_scaled, X_valid_scaled, X_test_scaled, y_train, y_valid, y_test = load_and_prep_data()
+    X_train_scaled, X_valid_scaled, X_test_scaled, y_train, y_valid, y_test = load_and_prep_data()
 
-    plot_loss_function()
+    # plot_loss_function()
+
+    input_shape = X_train_scaled.shape[1:]
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(30, activation="selu", kernel_initializer="lecun_normal", input_shape=input_shape),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(loss=huber_fn, optimizer="nadam", metrics=["mae"])
+    model.fit(X_train_scaled, y_train, epochs=2, validation_data=(X_valid_scaled, y_valid), workers=-1)
 
     print("custom loss")
