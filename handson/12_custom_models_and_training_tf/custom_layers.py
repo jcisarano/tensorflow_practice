ee31e3a4424d1d454827f98f10e36ab9f9ebc9ed
jcisarano <cisarano@gmail.com>
2022-03-04 08:11:52 -0500

    create custom dense layer class

--- handson/12_custom_models_and_training_tf/custom_layers.py
+++ handson/12_custom_models_and_training_tf/custom_layers.py
@@ -5,6 +5,34 @@ import numpy as np
 from custom_loss import load_and_prep_data
 
 
+class MyDense(tf.keras.layers.Layer):
+    def __init__(self, units, activation=None, **kwargs):
+        super().__init__(**kwargs)
+        self.units = units
+        self.activation = tf.keras.activations.get(activation)
+
+    def build(self, batch_input_shape):
+        self.kernel = self.add_weight(
+            name="kernel", shape=[batch_input_shape[-1], self.units],
+            initializer="glorot_normal"
+        )
+        self.bias = self.add_weight(
+            name="bias", shape=[self.units], initializer="zeros"
+        )
+        super().build(batch_input_shape)  # must be at end
+
+    def call(self, X):
+        return self.activation(X @ self.kernel + self.bias)
+
+    def compute_output_shape(self, batch_input_shape):
+        return tf.TensorShape(batch_input_shape.as_list()[:-1]+[self.units])
+
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config, "units": self.units,
+                "activation": tf.keras.activations.serialize(self.activation)}
+
+
 def cust_exp_layer(X_train_scaled, X_valid_scaled, X_test_scaled, y_train, y_valid, y_test, input_shape):
     exponential_layer = tf.keras.layers.Lambda(lambda x: tf.exp(x))
     print(exponential_layer([-1., 0., 1.]))
