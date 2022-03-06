1f98cd91345d9d37a2e3c775690a8b2eecbceca2
jcisarano <cisarano@gmail.com>
2022-03-06 11:17:23 -0500

    added ResidualRegressor class

--- handson/12_custom_models_and_training_tf/custom_models.py
+++ handson/12_custom_models_and_training_tf/custom_models.py
@@ -19,6 +19,23 @@ class ResidualBlock(keras.layers.Layer):
             return inputs + Z
 
 
+class ResidualRegressor(keras.models.Model):
+    def __init__(self, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.hidden1 = keras.layers.Dense(30, activation="elu",
+                                          kernel_initializer="he_normal")
+        self.block1 = ResidualBlock(2, 30)
+        self.block2 = ResidualBlock(2, 30)
+        self.out = keras.layers.Dense(output_dim)
+
+    def call(self, inputs):
+        Z = self.hidden1(inputs)
+        for _ in range(1+3):
+            Z = self.block1(Z)
+        Z = self.block2(Z)
+        return self.out(Z)
+
+
 def run():
     X_train_scaled, X_valid_scaled, X_test_scaled, y_train, y_valid, y_test = load_and_prep_data()
     input_shape = X_train_scaled.shape[1:]
