8fdfabb71f244c94b5815cd3dce63974b57cef17
jcisarano <cisarano@gmail.com>
2022-03-06 11:26:25 -0500

    fit model

--- handson/12_custom_models_and_training_tf/custom_models.py
+++ handson/12_custom_models_and_training_tf/custom_models.py
@@ -5,11 +5,11 @@ import keras
 from custom_loss import load_and_prep_data
 
 
-class ResidualBlock(keras.layers.Layer):
+class ResidualBlock(tf.keras.layers.Layer):
     def __init__(self, n_layers, n_neurons, **kwargs):
         super().__init__(**kwargs)
-        self.hidden = [keras.layers.Dense(n_neurons, activation="elu",
-                                          kernel_initializer="he_normal")
+        self.hidden = [tf.keras.layers.Dense(n_neurons, activation="elu",
+                                             kernel_initializer="he_normal")
                        for _ in range(n_layers)]
 
     def call(self, inputs):
@@ -19,18 +19,18 @@ class ResidualBlock(keras.layers.Layer):
             return inputs + Z
 
 
-class ResidualRegressor(keras.models.Model):
+class ResidualRegressor(tf.keras.models.Model):
     def __init__(self, output_dim, **kwargs):
         super().__init__(**kwargs)
-        self.hidden1 = keras.layers.Dense(30, activation="elu",
-                                          kernel_initializer="he_normal")
+        self.hidden1 = tf.keras.layers.Dense(30, activation="elu",
+                                             kernel_initializer="he_normal")
         self.block1 = ResidualBlock(2, 30)
         self.block2 = ResidualBlock(2, 30)
-        self.out = keras.layers.Dense(output_dim)
+        self.out = tf.keras.layers.Dense(output_dim)
 
     def call(self, inputs):
         Z = self.hidden1(inputs)
-        for _ in range(1+3):
+        for _ in range(1 + 3):
             Z = self.block1(Z)
         Z = self.block2(Z)
         return self.out(Z)
@@ -38,13 +38,16 @@ class ResidualRegressor(keras.models.Model):
 
 def run():
     X_train_scaled, X_valid_scaled, X_test_scaled, y_train, y_valid, y_test = load_and_prep_data()
-    input_shape = X_train_scaled.shape[1:]
     X_new_scaled = X_test_scaled
 
     keras.backend.clear_session()
     np.random.seed(42)
     tf.random.set_seed(42)
 
-
+    model = ResidualRegressor(1)
+    model.compile(loss="mse", optimizer="nadam")
+    history = model.fit(X_train_scaled, y_train, epochs=5)
+    score = model.evaluate(X_test_scaled, y_test)
+    y_pred = model.predict(X_new_scaled)
 
     print("custom models")
