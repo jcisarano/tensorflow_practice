8b79def510c3506856893045cfff5eb78966a8ae
jcisarano <cisarano@gmail.com>
2022-03-08 08:19:28 -0500

    linear classification example

--- cert_examples/binary_classification.py
+++ cert_examples/binary_classification.py
@@ -14,6 +14,18 @@ import tensorflow as tf
 from utils import generate_circles
 
 
+def simple_linear(X, y):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(100),
+        tf.keras.layers.Dense(10),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+                  optimizer=tf.keras.optimizers.Adam(),
+                  metrics=["accuracy"])
+    model.fit(X, y, epochs=100, workers=-1)
+
+
 def binary_classification_nonlinear(X, y):
     """
     Simple binary classification good for nonlinear data
@@ -22,7 +34,7 @@ def binary_classification_nonlinear(X, y):
     :return:
     """
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(4, activation="relu"),
+        tf.keras.layers.Dense(4, activation="relu"),  # nonlinear activation function
         tf.keras.layers.Dense(4, activation="relu"),
         tf.keras.layers.Dense(1, activation="sigmoid")
     ])
