4ec0b52fb0043b8dbcdbba27cc20aeffc17616c3
jcisarano <cisarano@gmail.com>
2022-03-08 08:03:30 -0500

    nonlinear binary classification example

--- cert_examples/binary_classification.py
+++ cert_examples/binary_classification.py
@@ -1,18 +1,41 @@
+"""
+Common ways to improve model performance:
+    Adding layers
+    Increase the number of hidden units in the layers
+    Change the activation functions of the layers
+    Change the optimization function of the model
+    Change the learning rate of the optimization function
+    Fit on more data
+    Fit for longer
+"""
+
 import tensorflow as tf
 
 from utils import generate_circles
 
 
-def simple_binary_classification(X, y):
+def binary_classification_nonlinear(X, y):
+    """
+    Simple binary classification good for nonlinear data
+    :param X:
+    :param y:
+    :return:
+    """
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(1)
+        tf.keras.layers.Dense(4, activation="relu"),
+        tf.keras.layers.Dense(4, activation="relu"),
+        tf.keras.layers.Dense(1, activation="sigmoid")
     ])
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+                  optimizer=tf.keras.optimizers.Adam(),
+                  metrics=["accuracy"])
+    model.fit(X, y, epochs=50, workers=-1)
 
 
 def run():
     X, y = generate_circles()
 
     tf.random.set_seed(42)
-    simple_binary_classification(X, y)
+    binary_classification_nonlinear(X, y)
 
     print("binary classification")
