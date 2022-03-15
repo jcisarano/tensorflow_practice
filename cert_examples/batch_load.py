de5b7ee03bdc3c0fba77c9599e0b5d8f8a785432
jcisarano <cisarano@gmail.com>
2022-03-15 08:54:56 -0400

    added image batch load example

--- cert_examples/batch_load.py
+++ cert_examples/batch_load.py
@@ -1,4 +1,9 @@
-from transfer_learning import load_and_prep_data
+import tensorflow as tf
+from tensorflow_datasets.video.bair_robot_pushing import IMG_SHAPE
+
+from transfer_learning import load_and_prep_data, TRAIN_DATA_PATH, TEST_DATA_PATH
+
+
 # import tensorflow_datasets as tfds
 
 
@@ -6,10 +11,23 @@ from transfer_learning import load_and_prep_data
 #     datasets_list = tfds.list_builders()
 
 
-
 def run():
-    # loads in batches
-    train_data, test_data = load_and_prep_data()
+    # loads in batches using ImageGenerator
+    # train_data, test_data = load_and_prep_data()
 
+    # more current version, loads image data without need to create ImageGenerator
+    train_data = tf.keras.preprocessing.image_dataset_from_directory(TRAIN_DATA_PATH,
+                                                                     label_mode="categorical",
+                                                                     image_size=(224, 224),
+                                                                     batch_size=32)
+    test_data = tf.keras.preprocessing.image_dataset_from_directory(TEST_DATA_PATH,
+                                                                    label_mode="categorical",
+                                                                    image_size=(224,224),
+                                                                    batch_size=32)
+
+    print(train_data)
+    print(test_data)
+
+    print(train_data.class_names)
 
     print("batch load")
