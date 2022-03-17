a76eeb6f853cfc581be1a1ca13bb473f57620414
jcisarano <cisarano@gmail.com>
2022-03-17 09:01:03 -0400

    added another dataset example

--- cert_examples/tf_datasets.py
+++ cert_examples/tf_datasets.py
@@ -1,11 +1,15 @@
-
-from tensorflow.keras.datasets import fashion_mnist
+import tensorflow as tf
 
 import tensorflow_datasets as tfds
 
 
 def load_fashion_mnist():
-    (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+
+
+def load_boston_housing():
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.boston_housing.load_data(path="boston_housing.npz",
+                                                                                      test_split=0.2, seed=42)
 
 
 def load_mnist():
