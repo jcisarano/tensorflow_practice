e449098fac7a5fdedf108f8af12de68a79720d09
jcisarano <cisarano@gmail.com>
2022-03-18 08:25:07 -0400

    added note

--- cert_examples/load_data_from_csv_or_json.py
+++ cert_examples/load_data_from_csv_or_json.py
@@ -6,3 +6,9 @@ import pandas as pd
 def load_data(path, filename):
     csv_path = os.path.join(path, filename)
     return pd.read_csv(csv_path)
+
+
+"""
+json example?
+https://stackoverflow.com/questions/38381887/how-to-read-json-files-in-tensorflow
+"""
