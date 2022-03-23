14f5299c8b010d0f3ab9fabe37c6e439c6cd9d81
jcisarano <cisarano@gmail.com>
2022-03-23 08:20:35 -0400

    added custom model

--- cert_examples/nlp_text_gen.py
+++ cert_examples/nlp_text_gen.py
@@ -1,5 +1,7 @@
 """
 Given a character, or sequence of characters, what is the most probable next character?
+
+Uses GRU, but could be swapped out with LSTM
 """
 
 import tensorflow as tf
@@ -37,6 +39,30 @@ def split_input_target(sequence):
     input_text = sequence[:-1]
     target_text = sequence[1:]
     return input_text, target_text
+
+
+class MyModel(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, rnn_units):
+        super().__init(self)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(rnn_units,
+                                       return_sequences=True,
+                                       return_state=True)
+        self.dense = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, inputs, states=None, return_state=False, training=False):
+        x = inputs
+        x = self.embedding(x, training=training)
+        if states is None:
+            states = self.gru.get_initial_state(x)
+        x, states = self.gru(x, initial_state=states, training=training)
+        x = self.dense(x, training=training)
+
+        if return_state:
+            return x, states
+        else:
+            return x
+
 
 
 def run():
@@ -85,6 +111,10 @@ def run():
         .batch(BATCH_SIZE, drop_remainder=True)
         .prefetch(tf.data.experimental.AUTOTUNE)
     )
+
+    vocab_size = len(vocab)
+    embedding_dim = 256
+    rnn_units = 1024
 
 
     print("nlp text gen")
