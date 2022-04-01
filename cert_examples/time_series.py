9e7e34ec5af05a1e42b50f7f820525582a46aefd
jcisarano <cisarano@gmail.com>
2022-04-01 08:15:16 -0400

    added dense model for time series

--- cert_examples/time_series.py
+++ cert_examples/time_series.py
@@ -7,8 +7,11 @@ import pandas as pd
 DATA_PATH: str = "datasets/time_series"
 FILENAME: str = "BTC_USD_2013-10-01_2021-05-18-CoinDesk.csv"
 
-HORIZON: int = 1
-WINDOW_SIZE: int = 7
+HORIZON: int = 1  # horizon size can change to change preds
+WINDOW_SIZE: int = 7  # window size can change to increase/decrease amount of data per label
+
+CHECKPOINT_PATH: str = "models/time_series_checkpoints/"
+
 
 def load_dataframe(data_path):
     df = pd.read_csv(data_path,
@@ -28,7 +31,7 @@ def load_data():
 
 
 def my_train_test_split(X, y, split=0.8):
-    split_size = int(split*len(y))
+    split_size = int(split * len(y))
     X_train, y_train = X[:split_size], y[:split_size]
     X_test, y_test = X[split_size:], y[split_size:]
 
@@ -47,8 +50,8 @@ def make_windows(x, window_size, horizon):
     :param horizon:
     :return:
     """
-    window_step = np.expand_dims(np.arange(window_size+horizon), axis=0)
-    window_indexes = window_step + np.expand_dims(np.arange(len(x)-(window_size+horizon-1)), axis=1)
+    window_step = np.expand_dims(np.arange(window_size + horizon), axis=0)
+    window_indexes = window_step + np.expand_dims(np.arange(len(x) - (window_size + horizon - 1)), axis=1)
     windowed_array = x[window_indexes]
     windows, labels = get_labelled_windows(windowed_array, horizon=horizon)
 
@@ -56,13 +59,41 @@ def make_windows(x, window_size, horizon):
 
 
 def make_train_test_splits(windows, labels, test_split=0.2):
-    split_size = int(len(windows)*(1-test_split))
+    split_size = int(len(windows) * (1 - test_split))
     train_windows = windows[:split_size]
     train_labels = labels[:split_size]
     test_windows = windows[split_size:]
     test_labels = labels[split_size:]
 
     return train_windows, test_windows, train_labels, test_labels
+
+
+def create_model_checkpoint(model_name, save_path=CHECKPOINT_PATH):
+    return tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(save_path, model_name),
+                                              verbose=0,
+                                              save_best_only=True)
+
+
+def make_dense_model(model_name, train_windows, test_windows, train_labels, test_labels, output_size=HORIZON):
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(output_size, activation="linear")
+        ], name=model_name
+    )
+    model.compile(loss="mae",
+                  optimizer=tf.keras.optimizers.Adam(),
+                  metrics=["mae"])
+    model.fit(x=train_windows,
+              y=train_labels,
+              epochs=100,
+              verbose=1,
+              batch_size=128,
+              validation_data=(test_windows, test_labels),
+              callbacks=[create_model_checkpoint(model_name=model.name)],
+              workers=-1)
+
+    return model
 
 
 def run():
@@ -72,6 +103,6 @@ def run():
     train_windows, test_windows, train_labels, test_labels = make_train_test_splits(full_windows, full_labels)
 
     tf.random.set_seed(42)
-
+    model = make_dense_model("dense_model", train_windows, test_windows, train_labels, test_labels)
 
     print("time series")
