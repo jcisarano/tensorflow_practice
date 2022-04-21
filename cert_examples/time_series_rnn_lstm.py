617e00a0ecf00b0f8ffda80ba2d36ff6b069e647
jcisarano <cisarano@gmail.com>
2022-04-21 08:28:40 -0400

    set up for lstm model create example

--- cert_examples/time_series_rnn_lstm.py
+++ cert_examples/time_series_rnn_lstm.py
@@ -1,7 +1,12 @@
+import tensorflow as tf
 from time_series import load_data, make_windows, make_train_test_splits
 
 HORIZON: int = 1
 WINDOW_SIZE: int = 7
+
+
+def make_lstm_model(model_name, train_windows, test_windows, train_labels, test_labels, output_size):
+    print("make model")
 
 
 def run():
@@ -9,5 +14,8 @@ def run():
     full_windows, full_labels = make_windows(prices, window_size=WINDOW_SIZE, horizon=HORIZON)
     train_windows, test_windows, train_labels, test_labels = make_train_test_splits(full_windows, full_labels)
 
-    
+    tf.random.set_seed(42)
+    model_name = "time_series_rnn_lstm"
+    make_lstm_model(model_name, train_windows, test_windows, train_labels, test_labels, HORIZON)
+
     print("time series rnn")
